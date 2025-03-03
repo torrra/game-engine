d95837e212ddb5e72ba53efa2af180db6ff9c4ef
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-03 14:21:30 -0500

    Add CreateComponent and GetComponenet in ComponentArray
    Add functions to create, get and invalidate components to ComponentArray class
    Edit MoveReparentedComponent function to actually move comonent to end of array
    instead of just a print

--- sceneGraphProject/header/ComponentArray.h
+++ sceneGraphProject/header/ComponentArray.h
@@ -9,7 +9,10 @@ namespace engine
 	{
 	public:
 
-		void MoveReparentedComponent(EntityHandle owner);
+		void			MoveReparentedComponent(EntityHandle owner);
+		void			InvalidateComponent(EntityHandle owner);
+		TComponentType* CreateComponent(EntityHandle owner, EntityHandle parent);
+		TComponentType* GetComponent(EntityHandle owner);
 
 	private:
 
@@ -28,5 +31,58 @@ namespace engine
 		}
 
 		printf("yea we moving the component\n");
+
+		if ((!m_entityIndexMap.contains(owner)) || m_components.empty())
+			return;
+
+		uint64				newIndex = m_components.size();
+		TComponentType&		toMove = m_components[m_entityIndexMap[owner]];
+
+		m_components.emplace_back(toMove);
+		toMove.Invalidate();
+
+	}
+
+	template<CValidComponent TComponentType>
+	inline void ComponentArray<TComponentType>::InvalidateComponent(EntityHandle owner)
+	{
+		m_components[m_entityIndexMap[owner]].Invalidate();
+		m_entityIndexMap.erase(owner);
+	}
+
+	template<CValidComponent TComponentType>
+	inline TComponentType* ComponentArray<TComponentType>::CreateComponent(EntityHandle owner, EntityHandle parent)
+	{		
+		EntityHandle    parentIndex = 0;
+
+		if (m_entityIndexMap.contains(parent))
+			parentIndex = m_entityIndexMap[parent];
+
+		for (EntityHandle newIndex = 0; newIndex < m_components.size(); ++newIndex)
+		{
+			TComponentType& currentComponent = m_components[newIndex];
+
+			if (currentComponent.IsValid() || parentIndex > newIndex)
+				continue;
+
+			printf("[Component array]: filling invalid slot\n");
+
+			currentComponent = TComponentType(owner);
+			m_entityIndexMap[owner] = newIndex;		
+			return &currentComponent;
+		}
+
+		printf("[Component array]: creating new slot\n");
+		m_entityIndexMap[m_components.size()] = owner;
+		return &m_components.emplace_back(owner);
+	}
+
+	template<CValidComponent TComponentType>
+	inline TComponentType* ComponentArray<TComponentType>::GetComponent(EntityHandle owner)
+	{
+		if (!m_entityIndexMap.contains(owner))
+			return nullptr;
+
+		return &m_components[m_entityIndexMap[owner]];
 	}
 }
