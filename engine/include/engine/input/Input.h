4276fdc1e29f6af6151ac4e839d5716308280bdc
Marc Levesque <m.levesque@student.isartdigital.com>
2025-03-03 16:09:06 -0500

    Add input manager
    Move input manager into engine dll

---
+++ engine/include/engine/input/Input.h
@@ -0,0 +1,130 @@
+#pragma once
+
+#include "InputData.h"
+
+#include <math/MathGeneric.hpp>
+#include <math/Vector2.hpp>
+
+#include <mutex>
+#include <string>
+#include <unordered_map>
+
+#include "utility/MemoryCheck.h"
+#include "EngineExport.h"
+
+/*
+*	--------- Keyboard Input ---------
+*	- Keyboard Key pressed		DONE
+*	- Keyboard Key held			DONE
+*	- Keyboard Key release		DONE
+*	- Mouse button press		DONE
+*	- Mouse button held			DONE
+*	- Mouse button release		DONE
+*	- Mouse position			DONE
+*	- Mouse delta position		DONE
+*	- Mouse delta scroll		DONE
+*	- Set mouse mode			DONE
+*	- Handle modifier keys		
+*	- Controller support?		
+*	- Input register			DONE
+*	- Remove singleton
+*/
+
+struct GLFWwindow;
+
+namespace engine
+{
+	enum EInputState
+	{
+		PRESSED = KEY_STATE_PRESSED,
+		HELD = KEY_STATE_HELD,
+		RELEASED = KEY_STATE_RELEASED,
+		NONE = KEY_STATE_NONE
+	};
+
+	enum ECursorMode
+	{
+		NORMAL = CURSOR_MODE_NORMAL,
+		HIDDEN = CURSOR_MODE_HIDDEN,
+		DISABLED = CURSOR_MODE_DISABLED,
+		CAPTURED = CURSOR_MODE_CAPTURED
+	};
+
+	class Input
+	{
+	public:
+								~Input(void) = default;
+
+		// Input manager functions
+		static void ENGINE_API RegisterInput(int key);
+		static void ENGINE_API UnregisterInput(int key);
+		static void ENGINE_API ShutDownInputManager(void);
+		static void ENGINE_API SetCursorMode(ECursorMode cursorMode);
+
+		// Mouse getter functions
+		template <math::CScalarType TValueType>
+		static math::Vector2<TValueType> ENGINE_API GetCursorPosition(void);
+
+		template <math::CScalarType TValueType>
+		static math::Vector2<TValueType> ENGINE_API GetCursorDeltaPos(void);
+
+		template <math::CScalarType TValueType>
+		static math::Vector2<TValueType> ENGINE_API GetScrollDelta(void);
+
+		// Input state functions
+		static bool ENGINE_API IsInputPressed(int input);
+		static bool ENGINE_API IsInputHeld(int input);
+		static bool ENGINE_API IsInputReleased(int input);
+
+		// Callback functions
+		static void ENGINE_API StaticKeyboardCallback(GLFWwindow* window, int key, int scanCode, int action, int mods);
+		static void ENGINE_API MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+		static void ENGINE_API MouseScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+		static void ENGINE_API CursorPosCallback(GLFWwindow* window, double xPos, double yPos);
+	private:
+						Input(void) = default;
+						Input(Input const& inputManager) = delete;
+		Input&			operator=(Input const& inputManager) = delete;
+		static bool		HasKey(int keyCode) noexcept;
+		static Input*	GetInstance(void);
+
+		std::unordered_map<int, EInputState> m_keyMap;
+
+		math::Vector2<double>					m_cursorPos;
+		math::Vector2<double>					m_scrollDelta;
+
+		static std::mutex						m_mutex;
+		static Input*							m_instance;
+	};
+
+	// Template function definitions
+	template<math::CScalarType TValueType>
+	inline math::Vector2<TValueType> Input::GetCursorPosition(void)
+	{
+		math::Vector2<double> cursorPos = Input::GetInstance()->m_cursorPos;
+
+		return math::Vector2<TValueType>(
+			static_cast<TValueType>(cursorPos.GetX()),
+			static_cast<TValueType>(cursorPos.GetY())
+		);
+	}
+
+	template<math::CScalarType TValueType>
+	inline math::Vector2<TValueType> Input::GetCursorDeltaPos(void)
+	{
+		static math::Vector2<TValueType> lastPos = GetCursorPosition<TValueType>();
+		math::Vector2<TValueType> cursorPos = GetCursorPosition<TValueType>();
+
+		math::Vector2<TValueType> deltaPos = cursorPos - lastPos;
+
+		lastPos = cursorPos;
+
+		return deltaPos;
+	}
+
+	template<math::CScalarType TValueType>
+	inline math::Vector2<TValueType> Input::GetScrollDelta(void)
+	{
+		return reinterpret_cast<TValueType>(m_scrollDelta);
+	}
+}
---
+++ engine/source/Input.cpp
@@ -0,0 +1,142 @@
+#include "input/Input.h"
+
+#include <GLFW/glfw3.h>
+
+std::mutex engine::Input::m_mutex;
+engine::Input* engine::Input::m_instance = nullptr;
+
+void engine::Input::RegisterInput(int key)
+{
+	// Check if key exists
+	if (HasKey(key))
+	{
+		std::printf("Key already registered\n");
+		return;
+	}
+
+	GetInstance()->m_keyMap[key] = EInputState::NONE;
+}
+
+void engine::Input::UnregisterInput(int key)
+{
+	// Check if key exists
+	if (!HasKey(key))
+	{
+		std::printf("Key is not registered\n");
+		return;
+	}
+
+	GetInstance()->m_keyMap.erase(key);
+}
+
+bool engine::Input::IsInputPressed(int input)
+{
+	// Key not registed
+	if (!GetInstance()->HasKey(input))
+		return false;
+
+	bool result = GetInstance()->m_keyMap[input] == EInputState::PRESSED;
+	if (result)
+		GetInstance()->m_keyMap[input] = EInputState::HELD;
+
+	return result;
+}
+
+bool engine::Input::IsInputHeld(int input)
+{
+	// Key not registed
+	if (!GetInstance()->HasKey(input))
+		return false;
+
+	EInputState keyState = GetInstance()->m_keyMap[input];
+
+	return
+		keyState != EInputState::NONE &&
+		keyState != EInputState::RELEASED;
+}
+
+bool engine::Input::IsInputReleased(int input)
+{
+	// Key not registed
+	if (!GetInstance()->HasKey(input))
+		return false;
+
+	bool result = GetInstance()->m_keyMap[input] == EInputState::RELEASED;
+	if (result)
+		GetInstance()->m_keyMap[input] = EInputState::NONE;
+
+	return result;
+}
+
+void engine::Input::StaticKeyboardCallback(GLFWwindow* window, int key, int scanCode, int action, int mods)
+{
+	/*
+	*	Keyboard Callback
+	*	Gets data for the state of every key
+	*
+	*	Key:		ASCII numeric key code
+	*	Scancode:	Platform specific key identifier
+	*	Action:		State of the key, can either be press, repeat or release
+	*	mods:		Checks if a modifier key is pressed, this can be the shift,
+	*				alt, ctrl or windows key
+	*/
+
+	// TODO: implement support for modifier keys
+	(void) window;
+	(void) scanCode;
+	(void) mods;
+
+	if (GetInstance()->m_keyMap.contains(key))
+		GetInstance()->m_keyMap[key] = static_cast<EInputState>(action);
+}
+
+void engine::Input::MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+	(void) window;
+	(void) mods;
+
+	if (GetInstance()->m_keyMap.contains(button))
+		GetInstance()->m_keyMap[button] = static_cast<EInputState>(action);
+}
+
+void engine::Input::MouseScrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+{
+	(void) window;
+
+	GetInstance()->m_scrollDelta = math::Vector2<double>(xOffset, yOffset);
+}
+
+void engine::Input::CursorPosCallback(GLFWwindow* window, double xPos, double yPos)
+{
+	(void) window;
+
+	GetInstance()->m_cursorPos = math::Vector2<double>(xPos, yPos);
+}
+
+void engine::Input::ShutDownInputManager(void)
+{
+	delete m_instance;
+}
+
+void engine::Input::SetCursorMode(ECursorMode cursorMode)
+{
+	glfwSetInputMode(glfwGetCurrentContext(), CURSOR, cursorMode);
+}
+
+bool engine::Input::HasKey(int key) noexcept
+{
+	return GetInstance()->m_keyMap.contains(key);
+}
+
+engine::Input* engine::Input::GetInstance(void)
+{
+	if (!m_instance)
+	{
+		std::unique_lock<std::mutex> lock(m_mutex);
+
+		if (!m_instance)
+			m_instance = new Input();
+	}
+
+	return m_instance;
+}
