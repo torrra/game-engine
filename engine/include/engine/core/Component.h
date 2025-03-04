97765219474e525465b6186d4756462630f1a22b
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-04 13:10:16 -0500

    Add dll interface to some Component member functions
    Add ENGINE_API macro to Component public member functions, except Update

--- engine/include/engine/core/Component.h
+++ engine/include/engine/core/Component.h
@@ -16,24 +16,29 @@ namespace engine
 
 	public:
 
-		Component(EntityHandle owner);
+		ENGINE_API Component(EntityHandle owner);
+		ENGINE_API ~Component(void) = default;
 
 		virtual void Update(void) = 0;
 
 		// Is this component a valid object?
 		// true: this object is a valid component instance
 		// false: this object is 'dead' and subject to overwrite
+		ENGINE_API
 		bool IsValid(void) const;
 
 		// Is this component included when updating the scene?
+		ENGINE_API
 		bool IsActive(void) const;
 
 		// Set the component's active status
 		// true: will be included in update
 		// false: will be excluded in update
+		ENGINE_API
 		void Activate(bool activeState);
 
 		// Permanently set object up for destruction
+		ENGINE_API
 		void Invalidate(void);
 
 	protected:
