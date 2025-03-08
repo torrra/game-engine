01428e848c9efb31a25102b852615cd1dd372710
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-08 11:32:15 -0500

    Add GetEntity lua function and rename functions in script
    Add a global function to get an entity from its full name, and add
    an underscore in front of private functions in Entity.lua

--- build/scripts/Entity.lua
+++ build/scripts/Entity.lua
@@ -3,17 +3,19 @@ Entity = {handle = 0, ref = nil}
 
 local ExistingEntities = {}
 
-currentEntity = {}
 
 function Entity:IsValid()
+
 	return EntityRef.IsValid(self.ref)
 end
 
 function Entity:RefreshRef()
+
 	self.ref = EntityRef.GetEntityRef(self.handle)
 end
 
-function Entity:NewNativeEntity(newHandle)
+function Entity:_NewNativeEntity(newHandle)
+
 	local newEntity = {handle = newHandle, ref = nil }
 	self.__index = self
 	setmetatable(newEntity, self)
@@ -29,8 +31,16 @@ function Entity:Activate(active)
 	end
 end
 
-function AddEntityHandle(newHandle)
-	ExistingEntities[newHandle] = Entity:NewNativeEntity(newHandle)
-	currentEntity = ExistingEntities[newHandle]
+function GetEntity(entityName)
+	return ExistingEntities[entityName]
 end
 
+
+function _AddEntityHandle(newHandle, entityName)
+
+	ExistingEntities[entityName] = Entity:_NewNativeEntity(newHandle)
+	print("\n[Entity script] successfully registered "..entityName.."\n")
+end
+
+
+return Entity
