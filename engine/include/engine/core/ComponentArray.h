c1bde1aa623e0c7e288de2ba560029bb24942483
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-09 18:08:46 -0400

    Add begin() and end() functions to ComponentArray class
    Add functions to allow for ranged loops in ComponentArray.h

--- engine/include/engine/core/ComponentArray.h
+++ engine/include/engine/core/ComponentArray.h
@@ -32,6 +32,15 @@ namespace engine
 		// Check if an entity owns a component in this array
 		bool			HasComponent(EntityHandle entity);
 
+
+		// Standard functions necessary to use ranged for loops.
+		// Cannot use PascalCase for these two functions as they must
+		// match a precise syntax. Their return types are STL vector
+		// iterators
+
+		auto begin(void);
+		auto end(void);
+
 	private:
 
 		// Create a component without checking if the object's parent is
@@ -158,6 +167,18 @@ namespace engine
 	inline bool ComponentArray<TComponentType>::HasComponent(EntityHandle entity)
 	{
 		return m_entityIndexMap.contains(entity);
+	}
+
+	template<CValidComponent TComponentType>
+	inline auto ComponentArray<TComponentType>::begin(void)
+	{
+		return m_components.begin();
+	}
+
+	template<CValidComponent TComponentType>
+	inline auto ComponentArray<TComponentType>::end(void)
+	{
+		return m_components.end();
 	}
 
 	template<CValidComponent TComponentType>
