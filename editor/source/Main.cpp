553d5874c44e751b38fcc98057e361d04c668948
Justine <jus.martin@student.isartdigital.com>
2025-03-10 14:40:29 -0400

    Delete unecessary transform tests

--- editor/source/Main.cpp
+++ editor/source/Main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 
-#include "mathsTests/TransformTest.h"
-
 int main(void)
 {
 	std::printf("Starting editor\n");
-
-	Editor::TransformConstructorTest();
-	Editor::TransformToMatrixTest();
-	Editor::TransformInterpolateTest();
-	Editor::TransformCopyTest();
-	Editor::TransformGetterTest();
-	Editor::TransformSetterTest();
 
 	return 0;
 }
