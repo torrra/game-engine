21caa6c60aaef5b43cd8e9c735e6539fe59c0316
Justine <jus.martin@student.isartdigital.com>
2025-03-10 14:40:01 -0400

    Change the content by previous transform file

--- engine/include/engine/core/components/Transform.h
+++ engine/include/engine/core/components/Transform.h
@@ -1,33 +1,80 @@
 #pragma once
 
+#pragma region math
+
 #include <math/Vector3.hpp>
 #include <math/Quaternion.hpp>
 
-#include "Component.h"
+#pragma endregion
+
+#pragma region core
+
+#include "core/Component.h"
+
+#pragma endregion
 
 namespace engine
 {
     class Transform final : public Component
     {
+	private:
+
+		enum E_TransformFlag : uint8
+		{
+			ABSOLUTE = 1
+		};
+
+	public:
 
 		using Component::Component;
 
-		void Update(void) override
-		{
+		ENGINE_API 						Transform(const Transform& inTransform) = default;
+		ENGINE_API 						Transform(Transform&& inTransform) = default;
 
-		}
+		/// Destructors
+		ENGINE_API 						~Transform(void) = default;
 
-    private:
+		/// Transform to matrix
+		ENGINE_API static lm::Matrix4f	ToMatrixWithoutScale(Transform& inTransform);
+		ENGINE_API static lm::Matrix4f	ToMatrixWithScale(Transform& inTransform);
+		ENGINE_API static Transform		Interpolate(Transform& inStartTransform,
+													Transform& inEndTransform,
+													float inTime);
+		ENGINE_API void					CopyPosition(const Transform& inTransform);
+		ENGINE_API void 				CopyRotation(const Transform& inTransform);
+		ENGINE_API void 				CopyScale(const Transform& inTransform);
 
-	    math::Quatf    m_localRotation;
-	    math::Quatf    m_absoluteRotation;
+		ENGINE_API void					Update(void) {}
 
-	    math::Vector3f m_localPosition;
-	    math::Vector3f m_localScale;
+		/// Getters
+		ENGINE_API lm::Vector3f			GetPosition(void) const;
+		ENGINE_API lm::Quatf			GetRotation(void) const;
+		ENGINE_API lm::Vector3f			GetScale(void) const;
+		ENGINE_API Transform			GetTransform(void) const;
 
-	    math::Vector3f m_absolutePosition;
-	    math::Vector3f m_absoluteScale;
-    };
+		/// Setters
+		ENGINE_API void					SetPosition(const lm::Vector3f& inPosition);
+		ENGINE_API void					SetRotation(const lm::Quatf& inRotation);
+		ENGINE_API void					SetScale(const lm::Vector3f& inScale);
+		ENGINE_API void 				SetTransform(const lm::Vector3f& inPosition,
+													 const lm::Quatf& inRotation,
+													 const lm::Vector3f& inScalein =
+													 lm::Vector3f(1.0f, 1.0f, 1.0f));
+
+		/// Operators
+		Transform&						operator=(const Transform& inTransform) = default;
+		Transform&						operator=(Transform&& inTransform) = default;
+		std::ostream&					operator<<(std::ostream& os);
+
+	private:
+
+		/// Private members
+		lm::Vector3f	m_position = lm::Vector3f(0.f, 0.f, 0.f);
+		lm::Quatf		m_rotation = lm::Quatf(1.f, 0.f, 0.f, 0.f);
+		lm::Vector3f	m_scale = lm::Vector3f(1.f, 1.f, 1.f);
+		uint8			m_transformFlags = 0;
+
+    }; // !Class Transform
 
 	template <>
 	struct UpdateAfterParent<Transform>
@@ -41,4 +88,4 @@ namespace engine
 		return TRANSFORM;
 	}
 
-}
+} // !Namespace engine
---
+++ engine/source/core/components/Transform.cpp
@@ -0,0 +1,103 @@
+#include "core/components/Transform.h"
+
+lm::Matrix4f engine::Transform::ToMatrixWithoutScale(Transform& inTransform)
+{
+	lm::Matrix4f transformMatrix = lm::TransformMatrix(inTransform.m_rotation, inTransform.m_position);
+
+	return transformMatrix;
+}
+
+lm::Matrix4f engine::Transform::ToMatrixWithScale(Transform& inTransform)
+{
+	lm::Matrix4f positionMatrix = lm::Matrix4f::PositionMatrix(inTransform.m_position);
+
+	lm::Matrix4f rotationMatrix = inTransform.m_rotation.RotationMatrix();
+
+	lm::Matrix4f scaleMatrix = lm::Matrix4f::ScaleMatrix(inTransform.m_scale);
+
+	lm::Matrix4f transformMatrix = (scaleMatrix * rotationMatrix) * positionMatrix;
+
+	return transformMatrix;
+}
+
+engine::Transform engine::Transform::Interpolate(Transform& inStartTransform,
+												 Transform& inEndTransform, float inTime)
+{
+	Transform result = Transform();
+
+	result.m_position = lm::Vector3f::Lerp(inStartTransform.m_position,
+		inEndTransform.m_position, inTime);
+
+	result.m_rotation = lm::Quatf::Slerp(inStartTransform.m_rotation,
+		inEndTransform.m_rotation, inTime);
+
+	result.m_scale = lm::Vector3f::Lerp(inStartTransform.m_scale,
+		inEndTransform.m_scale, inTime);
+
+	return result;
+}
+
+void engine::Transform::CopyPosition(const Transform& inTransform)
+{
+	m_position = inTransform.m_position;
+}
+
+void engine::Transform::CopyRotation(const Transform& inTransform)
+{
+	m_rotation = inTransform.m_rotation;
+}
+
+void engine::Transform::CopyScale(const Transform& inTransform)
+{
+	m_scale = inTransform.m_scale;
+}
+
+lm::Vector3f engine::Transform::GetPosition(void) const
+{
+	return m_position;
+}
+
+lm::Quatf engine::Transform::GetRotation(void) const
+{
+	return m_rotation;
+}
+
+lm::Vector3f engine::Transform::GetScale(void) const
+{
+	return m_scale;
+}
+
+engine::Transform engine::Transform::GetTransform(void) const
+{
+	return *this;
+}
+
+void engine::Transform::SetPosition(const lm::Vector3f& inPosition)
+{
+	m_position = inPosition;
+}
+
+void engine::Transform::SetRotation(const lm::Quatf& inRotation)
+{
+	m_rotation = inRotation;
+}
+
+void engine::Transform::SetScale(const lm::Vector3f& inScale)
+{
+	m_scale = inScale;
+}
+
+void engine::Transform::SetTransform(const lm::Vector3f& inPosition,
+	const lm::Quatf& inRotation, const lm::Vector3f& inScale)
+{
+	m_position = inPosition;
+	m_rotation = inRotation;
+	m_scale = inScale;
+}
+
+std::ostream& engine::Transform::operator<<(std::ostream& os)
+{
+	return os << m_position.X() << " " << m_position.Y() << " " << m_position.Z() << " "
+		<< m_rotation.W() << " " << m_rotation.X() << " " << m_rotation.Y() << " " << m_rotation.Z() << " "
+		<< m_scale.X() << " " << m_scale.Y() << " " << m_scale.Z();
+}
