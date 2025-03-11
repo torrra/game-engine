07836054f0b9d583b7b0772c39526e96a54d297e
Marc Levesque <m.levesque@student.isartdigital.com>
2025-03-11 14:43:59 -0400

    Add label UI component
    Add UI element which stores text with the following features:
    - Font & font size
    - Position
    - Color
    - Text wrapping

--- engine/include/engine/ui/elements/Label.h
+++ engine/include/engine/ui/elements/Label.h
@@ -1,23 +1,40 @@
 #pragma once
 
-#include "engine/ui/UIElement.h"
 #include "engine/EngineExport.h"
+#include "engine/CoreTypes.h"
+#include "engine/ui/UIElement.h"
 
 #include <string>
+
+/*
+*	------------------ TODO: label ------------------
+*	- Alignment (left, right, center)		
+*	- Options: bold, italic, underline		
+*	-------------------------------------------------
+*/
+
+struct ImFont;
 
 namespace engine
 {
 	class Label : public UIElement
 	{
 	public:
+		ENGINE_API Label(void) = delete;
 		ENGINE_API Label(const char* text);
 		ENGINE_API virtual ~Label(void) override;
 
 		ENGINE_API virtual void Render(void) override;
+		
+		ENGINE_API void SetTextColor(f32 red, f32 green, f32 blue, f32 alpha = 1.0f);
+		ENGINE_API void SetFont(const char* fontName, f32 fontSize);
+		ENGINE_API void WrapText(f32 maxWidthPx);
 
 	private:
-		math::Vector4f m_textColor;
 		std::string m_text;
-
+		math::Vector4f m_textColor;
+		ImFont* m_font;
+		f32 m_fontScale;
+		f32 m_textBoxWidth;
 	};
 }
--- engine/source/ui/UIManager.cpp
+++ engine/source/ui/UIManager.cpp
@@ -4,6 +4,7 @@
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_opengl3.h>
 #include <imgui/imgui_impl_glfw.h>
+
 #include "utility/MemoryCheck.h"
 
 
