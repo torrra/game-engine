eecc13e5e4b7d5262dda94d8f2fce6a6bb38cb2d
Justine <jus.martin@student.isartdigital.com>
2025-03-11 13:16:34 -0400

    Implement PhysicsSystem constructor to initialize

--- engine/include/engine/physics/PhysicsSystem.h
+++ engine/include/engine/physics/PhysicsSystem.h
@@ -9,19 +9,23 @@ namespace engine
 		/// Constructor
 		// Empty constructor to initialize the physics system
 		// PxFoundation, PxPvd, PxPhysics
-		PhysicsSystem(void);
+							PhysicsSystem(void);
 
 		/// Destructor
 		// Destroy all the physics system resources
-		~PhysicsSystem(void);
+							~PhysicsSystem(void);
+
+		/// Getter
+		class PxPhysics*	getPhysics(void) const;
 
 	private :
 
 		/// Private members
 		// Forward declaration to do encapsulation
 		// PxFoundation, PxPvd, PxPhysics
-		struct Impl;
-		Impl* m_Impl = nullptr;
+		class physx::PxFoundation*	m_Foundation	= nullptr;
+		class physx::PxPhysics*		m_Physics		= nullptr;
+		class physx::PxPvd*			m_Pvd			= nullptr;
 
 	}; // !Class PhysicsSystem
 } // !Namespace engine
--- engine/source/physics/PhysicsSystem.cpp
+++ engine/source/physics/PhysicsSystem.cpp
@@ -1,2 +1,23 @@
 #include "physics/PhysicsSystem.h"
 
+#pragma region PhysX
+
+#include <physx/PxPhysicsAPI.h>
+
+#pragma endregion
+
+static physx::PxDefaultAllocator gDefaultAllocatorCallback;
+static physx::PxDefaultErrorCallback gDefaultErrorCallback;
+
+engine::PhysicsSystem::PhysicsSystem(void)
+{
+	m_Foundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback,
+									  gDefaultErrorCallback);
+	m_Pvd = physx::PxCreatePvd(*m_Foundation);
+	physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 
+																				5425, 10);
+	m_Pvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+
+	m_Physics = PxCreatePhysics(PX_PHYSICS_VERSION, *m_Foundation, physx::PxTolerancesScale(),
+								true, m_Pvd);
+}
