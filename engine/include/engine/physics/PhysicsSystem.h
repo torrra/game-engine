2b04e716b1103e85a880a7da19db7f41df36a048
Justine <jus.martin@student.isartdigital.com>
2025-03-11 11:56:23 -0400

    Implement the basics for initialise system

--- engine/include/engine/physics/PhysicsSystem.h
+++ engine/include/engine/physics/PhysicsSystem.h
@@ -4,6 +4,24 @@ namespace engine
 {
 	class PhysicsSystem
 	{
+	public :
+
+		/// Constructor
+		// Empty constructor to initialize the physics system
+		// PxFoundation, PxPvd, PxPhysics
+		PhysicsSystem(void);
+
+		/// Destructor
+		// Destroy all the physics system resources
+		~PhysicsSystem(void);
+
+	private :
+
+		/// Private members
+		// Forward declaration to do encapsulation
+		// PxFoundation, PxPvd, PxPhysics
+		struct Impl;
+		Impl* m_Impl = nullptr;
 
 	}; // !Class PhysicsSystem
 } // !Namespace engine
