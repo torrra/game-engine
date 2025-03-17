72dc94d165f62ca7a36d64d45c754ceaeb3ebc0d
Marc Levesque <m.levesque@student.isartdigital.com>
2025-03-17 09:51:27 -0400

    Clean up canvas class
    Add comments & apply c++ norm to canvas class

--- engine/include/engine/ui/Canvas.h
+++ engine/include/engine/ui/Canvas.h
@@ -1,43 +1,61 @@
 #pragma once
 
+#include "engine/EngineExport.h"
+#include "engine/CoreTypes.h"
 #include "engine/ui/UIElement.h"
 #include "engine/ui/elements/Label.h"
 #include "engine/ui/elements/Image.h"
 #include "engine/ui/elements/ProgressBar.h"
 #include "engine/ui/elements/Button.h"
 #include "engine/ui/elements/Rect.h"
-#include "engine/EngineExport.h"
 
 #include <math/Vector2.hpp>
-#include <math/Vector4.hpp>
 
 #include <vector>
-#include <string>
 
 namespace engine
 {
+	/*
+		Canvas class
+		The canvas class is used for adding UI elements to the screen. It should
+		only be used for 'in-game' UI.
+	*/
 	class Canvas
 	{
 	public:
-		ENGINE_API Canvas(math::Vector2f const& windowSize, math::Vector4f const& color);
-		ENGINE_API ~Canvas(void) = default;
+		/*
+			Create a canvas
 
-		ENGINE_API void Render(void);
-		ENGINE_API void Clear(void);
+			Window size: size in pixels
+			Color: background color as RGBA all values are between 0 - 1
+		*/
+		ENGINE_API				Canvas(math::Vector2f const& windowSize, math::Vector4f const& color);
+		ENGINE_API				Canvas(void) = delete;
+		ENGINE_API				~Canvas(void) = default;
 
-		ENGINE_API Label* AddLabel(const char* text);
-		ENGINE_API Image* AddImage(const char* fileName);
-		ENGINE_API Button* AddButton(const char* text, std::function<void(void)> function);
+		// Render function used to display UI to screen. It should be placed at the end of the update loop.
+		ENGINE_API void			Render(void);
+
+		// Clear function used to destroy a canvas & clear all associated UI elements.
+		ENGINE_API void			Clear(void);
+
+		// Add UI elements
+		ENGINE_API Label*		AddLabel(const char* text); 
+		ENGINE_API Image*		AddImage(const char* fileName);
+		ENGINE_API Button*		AddButton(const char* text, std::function<void(void)> function);
 		ENGINE_API ProgressBar* AddProgressBar(math::Vector2f const& position, math::Vector2f const& size, math::Vector2f const& range);
-		ENGINE_API Rectangle* AddRectangle(math::Vector2f const& pos, math::Vector2f const& sizePx);
+		ENGINE_API Rectangle*	AddRectangle(math::Vector2f const& position, math::Vector2f const& size);
+
+		// Change canvas's background color. All values between 0 - 1.
+		ENGINE_API void			SetColor(f32 red, f32 green, f32 blue, f32 alpha);
 
 	private:
-		void RescaleCanvas(void);
+		void					RescaleCanvas(void);
 
-		math::Vector2f m_size;
-		math::Vector2f m_prevSize;
 		std::vector<UIElement*> m_elements;
-		math::Vector4<float> m_color;
-		int32 m_uid;
+		math::Vector2f			m_size;
+		math::Vector2f			m_prevSize;
+		uint32					m_bgColor;
+		int32					m_uidCounter;
 	};
 }
--- engine/source/ui/Canvas.cpp
+++ engine/source/ui/Canvas.cpp
@@ -3,21 +3,20 @@
 
 #undef new
 #include <imgui/imgui.h>
-#include <imgui/imgui_impl_glfw.h>
-#include <imgui/imgui_impl_opengl3.h>
 
 #include "utility/MemoryCheck.h"
 
-#include <iostream>
 #include <functional>
 
 engine::Canvas::Canvas(math::Vector2f const& windowSize, math::Vector4f const& color)
-	: m_size(windowSize), m_prevSize(windowSize), m_color(color), m_uid(0)
+	: m_size(windowSize), m_prevSize(windowSize), m_uidCounter(0)
 {
+	SetColor(color[0], color[1], color[2], color[3]);
 }
 
-ENGINE_API void engine::Canvas::Render(void)
+void engine::Canvas::Render(void)
 {
+	// Make canvas uninteractable & allow inputs to pass through
 	ImGuiWindowFlags flags =
 		ImGuiWindowFlags_NoDecoration |
 		ImGuiWindowFlags_NoMove |
@@ -31,11 +30,13 @@ ENGINE_API void engine::Canvas::Render(void)
 	ImGui::SetNextWindowViewport(viewport->ID);
 	ImGui::SetNextWindowPos(viewport->Pos);
 	ImGui::SetNextWindowSize(viewport->Size);
-	ImGui::PushStyleColor(ImGuiCol_WindowBg, m_color);
+	ImGui::PushStyleColor(ImGuiCol_WindowBg, m_bgColor);
 	ImGui::Begin("Canvas", nullptr, flags);
 
+	// Scale all UI elements to be consistent with screen size
 	RescaleCanvas();
 
+	// Update all UI elements on this canvas
 	for (UIElement* element : m_elements)
 	{
 		element->Render();
@@ -45,7 +46,7 @@ ENGINE_API void engine::Canvas::Render(void)
 	ImGui::PopStyleColor();
 }
 
-ENGINE_API void engine::Canvas::Clear(void)
+void engine::Canvas::Clear(void)
 {
 	for (UIElement* element : m_elements)
 	{
@@ -60,7 +61,7 @@ engine::Label* engine::Canvas::AddLabel(const char* text)
 	m_elements.push_back(new Label(text));
 
 	UIElement* element = m_elements[(int32) m_elements.size() - 1];
-	element->SetUID(++m_uid);
+	element->SetUID(++m_uidCounter);
 
 	return dynamic_cast<Label*>(element);
 }
@@ -70,7 +71,7 @@ engine::Image* engine::Canvas::AddImage(const char* fileName)
 	m_elements.push_back(new Image(fileName));
 
 	UIElement* element = m_elements[(int32) m_elements.size() - 1];
-	element->SetUID(++m_uid);
+	element->SetUID(++m_uidCounter);
 
 	return dynamic_cast<Image*>(element);
 }
@@ -80,7 +81,7 @@ engine::Button* engine::Canvas::AddButton(const char* text, std::function<void(v
 	m_elements.push_back(new Button(text, function));
 
 	UIElement* element = m_elements[(int32) m_elements.size() - 1];
-	element->SetUID(++m_uid);
+	element->SetUID(++m_uidCounter);
 	
 	return dynamic_cast<Button*>(element);
 }
@@ -90,36 +91,45 @@ engine::ProgressBar* engine::Canvas::AddProgressBar(math::Vector2f const& positi
 	m_elements.push_back(new ProgressBar(position, size, range));
 
 	UIElement* element = m_elements[(int32) m_elements.size() - 1];
-	element->SetUID(++m_uid);
+	element->SetUID(++m_uidCounter);
 
 	return dynamic_cast<ProgressBar*>(element);
 }
 
-engine::Rectangle* engine::Canvas::AddRectangle(math::Vector2f const& pos, math::Vector2f const& sizePx)
+engine::Rectangle* engine::Canvas::AddRectangle(math::Vector2f const& position, math::Vector2f const& size)
 {
-	m_elements.push_back(new Rectangle(pos, sizePx));
+	m_elements.push_back(new Rectangle(position, size));
 	
 	UIElement* element = m_elements[(int32) m_elements.size() - 1];
-	element->SetUID(++m_uid);
+	element->SetUID(++m_uidCounter);
 
 	return dynamic_cast<Rectangle*>(element);
 }
 
+void engine::Canvas::SetColor(f32 red, f32 green, f32 blue, f32 alpha)
+{
+	// Store color as int to reduce memory usage
+	m_bgColor = ImGui::ColorConvertFloat4ToU32({red, green, blue, alpha});
+}
+
 void engine::Canvas::RescaleCanvas(void)
 {
+	
 	m_size = ImGui::GetContentRegionAvail();
 
-	if (m_size != m_prevSize)
+	// Screen size has not changed
+	if (m_size == m_prevSize)
+		return;
+
+	const f32 regionRatio = m_size.GetX() / m_prevSize.GetX();
+
+	// Update scale for all UI elements
+	for (UIElement* element : m_elements)
 	{
-		const f32 regionRatio = m_size.GetX() / m_prevSize.GetX();
-
-		for (UIElement* element : m_elements)
-		{
-
-			if (element->m_autoScale)
-				element->AutoScale(regionRatio);
-		}
-
-		m_prevSize = m_size;
+		if (element->m_autoScale)
+			element->AutoScale(regionRatio);
 	}
+
+	// Update previous screen size
+	m_prevSize = m_size;
 }
