7964f21f846840447654be8cea474d96f68ed4ef
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-17 16:42:43 -0400

    Add ELoadStatus enum to IResource interface
    Add enum to know a multithread resource's status in IResource.h

--- engine/include/engine/resource/Resource.h
+++ engine/include/engine/resource/Resource.h
@@ -1,9 +1,23 @@
 #pragma once
+#include "engine/CoreTypes.h"
 
 namespace engine
 {
 	class IResource
 	{
+	protected:
+
+		// This enum only applies to resources loaded on a thread other
+		// than the main thread
+		enum ELoadStatus : uint8
+		{
+			// Resource was successfully loaded from another thread
+			LOADED = 1,
+
+			// Necessary graphics API calls were made
+			GRAPHICS_CALLS_COMPLETE = (uint8) (1 << 1)
+		};
+
 	public:
 						IResource(void) = default;
 		virtual			~IResource(void) = default;
