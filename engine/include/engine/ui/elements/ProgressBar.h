0edfce904fd9309ccf3436e9a333d84d36eb6ff7
Marc Levesque <m.levesque@student.isartdigital.com>
2025-03-17 15:57:54 -0400

    Clean up progress bar UI element class
    Apply c++ norm & add comments to code

--- engine/include/engine/ui/elements/ProgressBar.h
+++ engine/include/engine/ui/elements/ProgressBar.h
@@ -6,30 +6,27 @@
 #include "engine/utility/Colors.h"
 
 #include <math/Vector2.hpp>
-#include <math/Vector4.hpp>
-
-#include <string>
 
 namespace engine
 {
 	class ProgressBar : public UIElement
 	{
 	public:
-		ENGINE_API ProgressBar(void) = delete;
-		ENGINE_API ProgressBar(math::Vector2f const& position, math::Vector2f const& size, math::Vector2f const& range);
+		ENGINE_API			ProgressBar(void) = delete;
+		ENGINE_API			ProgressBar(math::Vector2f const& position, math::Vector2f const& size, math::Vector2f const& range);
 		ENGINE_API virtual ~ProgressBar(void) override = default;
 
-		ENGINE_API virtual void Render(void) override;
+		virtual void		Render(void) override;
 
-		ENGINE_API void SetValue(f32 value);
-		ENGINE_API void SetBgColor(f32 red, f32 green, f32 blue, f32 alpha);
-		ENGINE_API void SetFillColor(f32 red, f32 green, f32 blue, f32 alpha);
+		ENGINE_API void		SetValue(f32 value);
+		ENGINE_API void		SetBgColor(f32 red, f32 green, f32 blue, f32 alpha);
+		ENGINE_API void		SetFillColor(f32 red, f32 green, f32 blue, f32 alpha);
 	
 	private:
-		math::Vector2f m_range;
-		f32 m_value;
-		uint32 m_bgColor;
-		uint32 m_fillColor;
+		math::Vector2f		m_range;
+		f32					m_value;
+		uint32				m_bgColor;
+		uint32				m_fillColor;
 
 	};
 }
--- engine/source/ui/elements/ProgressBar.cpp
+++ engine/source/ui/elements/ProgressBar.cpp
@@ -4,17 +4,19 @@
 
 #undef new
 #include <imgui/imgui.h>
-#include <imgui/imgui_impl_glfw.h>
-#include <imgui/imgui_impl_opengl3.h>
-#include <imgui/imgui_internal.h>
+
+#define NO_TEXT "\0"
 
 engine::ProgressBar::ProgressBar(math::Vector2f const& position, math::Vector2f const& size, math::Vector2f const& range)
 	: m_range(range)
 {
-	m_transform.m_position = position;
-	m_transform.m_sizePx = size;
+	SetPosition(position);
+	SetScale(size);
+
+	// Default value
 	m_value = range[1];
 
+	// Default colors
 	SetBgColor(DARK_GRAY, 1.0f);
 	SetFillColor(LIGHT_GRAY, 1.0f);
 }
@@ -23,26 +25,25 @@ void engine::ProgressBar::Render(void)
 {
 	ImGui::PushStyleColor(ImGuiCol_FrameBg, m_bgColor);
 	ImGui::PushStyleColor(ImGuiCol_PlotHistogram, m_fillColor);
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 25.0f);
 	
 	ImGui::SetCursorPos(m_transform.m_position);
-	ImGui::ProgressBar(m_value, m_range, "\0");
+	ImGui::ProgressBar(m_value, m_range, NO_TEXT);
 
 	ImGui::PopStyleColor(2);
-	ImGui::PopStyleVar();
 }
 
 void engine::ProgressBar::SetValue(f32 value)
 {
+	// Limit value to progress bar range
 	m_value = math::Clamp(value, m_range[0], m_range[1]);
 }
 
 void engine::ProgressBar::SetBgColor(f32 red, f32 green, f32 blue, f32 alpha)
 {
-	m_bgColor = ImGui::ColorConvertFloat4ToU32({red, green, blue, alpha});;
+	m_bgColor = ImGui::ColorConvertFloat4ToU32({red, green, blue, alpha});
 }
 
 void engine::ProgressBar::SetFillColor(f32 red, f32 green, f32 blue, f32 alpha)
 {
-	m_fillColor = ImGui::ColorConvertFloat4ToU32({red, green, blue, alpha});;
+	m_fillColor = ImGui::ColorConvertFloat4ToU32({red, green, blue, alpha});
 }
