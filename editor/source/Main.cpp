3e260533437d6dfb43eb26262dda535ae2143b27
Justine <jus.martin@student.isartdigital.com>
2025-03-20 09:34:28 -0400

    Try to implement physics to see what to wrap

--- editor/source/Main.cpp
+++ editor/source/Main.cpp
@@ -2,6 +2,10 @@
 
 #include <engine/utility/MemoryCheck.h>
 
+#include "engine/physics/geometry/GeometryTest.h"
+
+#include <math/VectorGeneric.hpp>
+#include <math/Vector3.hpp>
 
 extern "C" {
 	_declspec(dllexport) unsigned int NvOptimusEnablement = 1;
@@ -14,7 +18,16 @@ int main(void)
 
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	{
-		
+		engine::Physics physics;
+
+		engine::RigidBody box(physics.GetPhysics(), physics.GetScene(), physics.GetMaterial(), math::Vector3f(-10.f, 2.f, 0.f));
+	
+		box.SetVelocity(math::Vector3f(10.f, 0.f, 0.f));
+
+		for (int i = 0; i < 300; ++i)
+		{
+			physics.StepSimulation(1.f / 60.f);
+		}
 	}
 
 	// Memory leak check
