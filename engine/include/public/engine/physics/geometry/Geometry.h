7771f473ac943df53b327344ee45fc2a41a42546
Justine <jus.martin@student.isartdigital.com>
2025-03-21 15:09:38 -0400

    Implement Geometry class and GeometryType enum

--- engine/include/public/engine/physics/geometry/Geometry.h
+++ engine/include/public/engine/physics/geometry/Geometry.h
@@ -6,23 +6,47 @@
 
 #pragma endregion
 
-#include "BoxGeometry.h"
+#pragma region Engine
+
+#include "engine/EngineExport.h"
+
+#pragma endregion
 
 namespace engine
 {
+	struct GeometryImpl;
+
+	enum EGeometryType
+	{
+		BOX,
+		SPHERE,
+		CAPSULE
+	};
+
 	class Geometry
 	{
 	public :
 
+		/// Constructor
+		// Default constructor set to a box geometry by default
+		ENGINE_API					Geometry(void);
+		// Constructor with enum parameter to choose the geometry
+		// Values are set by default
+		ENGINE_API					Geometry(EGeometryType geometryType);
+
 		/// Destructor
-		// Default virtual destructor
-		ENGINE_API virtual			~Geometry(void) = default;
+		ENGINE_API					~Geometry(void);
 
 		/// Getter
-		// Virtual getter for the native geometry
-		ENGINE_API virtual void*	GetNativeGeometry(void) const = 0;
-		// Virtual getter for the volume
-		ENGINE_API virtual float	GetVolume(void) const = 0;
+		ENGINE_API GeometryImpl&	GetGeometryImpl(void) const;
+
+		/// Public members
+		EGeometryType	m_geometryType = EGeometryType::BOX;
+
+	private :
+
+		/// Private members
+		GeometryImpl*	m_geometryImpl = nullptr;
 
 	}; // !Class Geometry
 } // !Namespace engine
