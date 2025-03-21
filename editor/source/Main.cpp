b7138e1a80211a34a59cf06b58df920f81f55527
Justine <jus.martin@student.isartdigital.com>
2025-03-21 13:51:23 -0400

    Test the update of rigidbody in regard to entity

--- editor/source/Main.cpp
+++ editor/source/Main.cpp
@@ -2,9 +2,11 @@
 
 #include "engine/physics/PhysicsEngine.h"
 #include "engine/physics/rigidbody/RigidBodyDynamic.h"
+#include "engine/physics/Material.h"
 
 #include <math/VectorGeneric.hpp>
 #include <math/Vector3.hpp>
+#include <math/Quaternion.hpp>
 
 #include "engine/core/SceneGraph.h"
 #include "engine/core/Entity.h"
@@ -25,36 +27,50 @@ int main(void)
 	{
 		engine::ScriptSystem::Startup();
 
+		/// ---------------- Create scene ----------------
 		engine::SceneGraph* scene = new engine::SceneGraph();
 
+		/// ---------------- Create entity ---------------- 
 		engine::EntityHandle entityHandle = scene->CreateEntity("First");
+		scene->CreateComponent<engine::Transform>(entityHandle)->SetPosition(math::Vector3f(0.f, 5.f, 0.f));
 
-		/// PhysicsEngine use
+		/// ---------------- PhysicsEngine use ---------------- 
 		engine::PhysicsEngine* physics;
 		physics->Get().Init();
 
+		/// ---------------- Create material ----------------
+		engine::Material* material = new engine::Material(physics->Get());
+		material->SetRestitution(1.f);
+
+		/// ---------------- Create rigidbody ----------------
 		engine::RigidBodyDynamic* rigidBody = new engine::RigidBodyDynamic(entityHandle, scene);
 		engine::Transform* transform = scene->GetComponent<engine::Transform>(entityHandle);
 		if (transform != nullptr)
 		{
-			rigidBody->CreateDynamicRigidBody(physics->Get(), *transform);
+			rigidBody->CreateDynamicRigidBody(physics->Get(), *transform, *material);
 			std::cout << "RigidBodyDynamic created, with existing transform" << std::endl;
 		}
 		else
 		{
 			engine::Transform* transform2 = scene->CreateComponent<engine::Transform>(entityHandle);
-			rigidBody->CreateDynamicRigidBody(physics->Get(), *transform2);
+			rigidBody->CreateDynamicRigidBody(physics->Get(), *transform2, *material);
 			std::cout << "RigidBodyDynamic created, with created transform" << std::endl;
 		}
 
+		/// ---------------- Simulation loop ---------------- 
 		for (int i = 0; i < 300; ++i)
 		{
 			physics->Get().StepSimulation(1.f / 60.f);
+			//rigidBody->UpdateEntity();
+			scene->GetComponent<engine::Transform>(entityHandle)->AddRotation(0.f, 0.f, 10.f);
+			rigidBody->UpdateRigidBody(*scene->GetComponent<engine::Transform>(entityHandle));
+
 		}
 
+		/// ---------------- Clean ---------------- 
 		delete rigidBody;
 		physics->Get().CleanUp();
-
+		delete material;
 		delete scene;
 
 		engine::ScriptSystem::Shutdown();
