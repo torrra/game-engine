292c6e53734eced898aa3288172996e4fdb39b83
Marc Levesque <m.levesque@student.isartdigital.com>
2025-03-21 16:19:26 -0400

    Modify controller script
    Invert movement directions

--- build/userScripts/ControllerScript.lua
+++ build/userScripts/ControllerScript.lua
@@ -11,23 +11,17 @@ end
 -- Is executed every tick
 function ControllerScript:Update(deltaTime)
     if IsInputDown(InputCode.KEY_W) then
-        self.transform:AddTranslation(0, 0, -1 * deltaTime) 
+        self.transform:AddTranslation(0, 0, 1 * deltaTime) 
     
     elseif IsInputDown(InputCode.KEY_S) then
-        self.transform:AddTranslation(0, 0,  1 * deltaTime)
+        self.transform:AddTranslation(0, 0, -1 * deltaTime)
     end
 
     if IsInputDown(InputCode.KEY_A) then
-        self.transform:AddTranslation(-1 * deltaTime, 0, 0)
+        self.transform:AddTranslation(1 * deltaTime, 0, 0)
     
     elseif IsInputDown(InputCode.KEY_D) then
-        self.transform:AddTranslation( 1 * deltaTime, 0, 0)
-    end
-
-    if IsInputDown(InputCode.KEY_Q) then
-        self.transform:AddEulerRotation(0, -50 * deltaTime, 0)
-    elseif IsInputDown(InputCode.KEY_E) then
-        self.transform:AddEulerRotation(0, 50 * deltaTime, 0)
+        self.transform:AddTranslation(-1 * deltaTime, 0, 0)
     end
 end
 
