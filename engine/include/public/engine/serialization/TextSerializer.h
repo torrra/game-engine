0b8a942cb5577d569c42cedd674d9d09c073e9b0
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-25 16:08:23 -0400

    Add new deserialize functions
    Add functions to deserialize data faster in TextSerializer

--- engine/include/public/engine/serialization/TextSerializer.h
+++ engine/include/public/engine/serialization/TextSerializer.h
@@ -13,6 +13,8 @@
 #include <math/Vector4.hpp>
 #include <math/Quaternion.hpp>
 
+#define MOVE_TEXT_CURSOR(text, end) text = text::GetValuePointer(text, end);\
+if (text == end) return text
 
 namespace engine
 {
@@ -109,19 +111,65 @@ namespace engine::text
 	} && !std::is_pointer_v<TEvaluatedType>;
 
 	template <CSerializableType TValueType>
-	void Serialize(std::ofstream& file, const char* name, const TValueType& val);
+	void Serialize(std::ostream& file, const char* name, const TValueType& val);
 
-	void Serialize(std::ofstream& file, const char* name, const std::string& val);
-	void Serialize(std::ofstream& file, const char* name, const char* val, uint64 length);
+	void Serialize(std::ostream& file, const char* name, const std::string& val);
+	void Serialize(std::ostream& file, const char* name, const char* val, uint64 length);
 
 	template <CSerializableType TValueType>
-	void Deserialize(std::ifstream& file, TValueType& val);	
+	void Deserialize(std::istream& file, TValueType& val);	
 
-	void Deserialize(std::ifstream& file, std::string& val);
+	void Deserialize(std::istream& file, std::string& val);
 
 	void MoveCursorToVal(uint64& pos, const std::string& line);
-	void MoveCursorToVal(std::ifstream& file);
+	void MoveCursorToVal(std::istream& file);
+
+	template <math::CIntegralType TValueType>
+	const char* DeserializeInteger(const char* text, TValueType& val);
+	
+	template<math::CFloatingType TValueType>
+	const char* DeserializeReal(const char* text, TValueType& val);
+
+	template <uint8 TDimensionsUint8, math::CScalarType TValueType>
+	const char* DeserializeVector(const char* text, math::Vector<TDimensionsUint8, TValueType>& val);
+
+	template<math::CFloatingType TValueType>
+	const char* DeserializeQuaternion(const char* text, math::Quaternion<TValueType>& val);
+
+	const char* DeserializeString(const char* text, const char* end, std::string & val);
+
+	const char* LoadFileData(std::ifstream& file, const char*& start, const char*& end);
+	void UnloadFileData(const char* data);
+
+	// Equivalent to MoveCursorToVal, but with another name
+	// to avoid confusion
+	const char* GetValuePointer(const char* text, const char* end);
+	const char* GetNewLine(const char* text, const char* end);
 }
+
+
+namespace engine::text::parser
+{
+	template <math::CScalarType TValueType>
+	TValueType CharToDigit(char character);
+
+	bool IsDigit(char character);
+	bool IsNewline(char character);
+	bool IsWhitespace(char character);
+
+	template <math::CScalarType TValueType>
+	const char* GetValSign(const char* text, TValueType& sign);
+
+	template <math::CFloatingType TValueType>
+	const char* ParseIntegralPart(const char* ptr, TValueType& result);
+
+	template <math::CFloatingType TValueType>
+	const char* ParseDecimalPart(const char* ptr, TValueType& result);
+
+	template <math::CFloatingType TValueType>
+	const char* ParseExponent(const char* ptr, TValueType& exponent);
+}
+
 
 namespace engine::text::types
 {
@@ -132,7 +180,7 @@ namespace engine::text::types
 namespace engine::text
 {
 	template<CSerializableType TValueType> inline
-	void Serialize(std::ofstream& file, const char* name, const TValueType& val)
+	void Serialize(std::ostream& file, const char* name, const TValueType& val)
 	{
 		using TNameType = std::remove_reference_t<std::remove_const_t<TValueType>>;
 
@@ -141,10 +189,102 @@ namespace engine::text
 	}
 
 	template<CSerializableType TValueType>
-	void Deserialize(std::ifstream& file, TValueType& val)
+	void Deserialize(std::istream& file, TValueType& val)
 	{
 		file >> val;
 	}
+
+	template<math::CIntegralType TValueType>
+	const char* DeserializeInteger(const char* text, TValueType& val)
+	{
+		val = 0;
+		TValueType sign;
+		text = parser::GetValSign(text, sign);
+		
+		while (parser::IsDigit(*text))
+		{
+			val = val * static_cast<TValueType>(10) + parser::CharToDigit<TValueType>(*text);
+			++text;
+		}
+
+		val *= sign;
+		return text;
+	}
+
+
+	template<math::CFloatingType TValueType>
+	const char* DeserializeReal(const char* text, TValueType& val)
+	{
+		TValueType   sign = static_cast<TValueType>(1);
+		text = parser::GetValSign(text, sign);
+
+		TValueType   parsed = static_cast<TValueType>(0);
+		TValueType   decimal = static_cast<TValueType>(0);
+		text = parser::ParseIntegralPart(text, parsed);
+
+		if ('.' == *text)
+			++text;
+
+		text = parser::ParseDecimalPart(text, decimal);
+		parsed += decimal;
+
+		if ('e' == *text || 'E' == *text)
+		{
+			TValueType exponent = static_cast<TValueType>(0);
+			text = parser::ParseExponent(text, exponent);
+			parsed *= exponent;
+		}
+		parsed *= sign;
+		val = parsed;
+		return text;
+	}
+
+
+	template<uint8 TDimensionsUint8, math::CScalarType TValueType>
+	const char* DeserializeVector(const char* text, math::Vector<TDimensionsUint8, TValueType>& val)
+	{
+		++text;
+
+		if constexpr (std::is_integral<TValueType>::value)
+		{
+			for (uint8 index = 0; index < TDimensionsUint8; ++index)
+			{
+				text = DeserializeInteger<TValueType>(text, val[static_cast<int32>(index)]);
+				++text;
+			}
+		}
+		else
+		{
+			for (uint8 index = 0; index < TDimensionsUint8; ++index)
+			{
+				text = DeserializeReal<TValueType>(text, val[static_cast<int32>(index)]);
+				++text;
+			}
+
+		}
+		return text;
+	}
+
+
+	template<math::CFloatingType TValueType>
+	const char* DeserializeQuaternion(const char* text, math::Quaternion<TValueType>& val)
+	{
+		++text;
+		text = DeserializeReal<TValueType>(text, val.W());
+		++text;
+
+		text = DeserializeReal<TValueType>(text, val.X());
+		++text;
+
+		text = DeserializeReal<TValueType>(text, val.Y());
+		++text;
+
+		text = DeserializeReal<TValueType>(text, val.Z());
+		++text;
+
+		return text;
+	}
+
 }
 
 
@@ -457,4 +597,109 @@ namespace engine::text::types
 		return "quat64";
 	}
 
+}
+
+
+namespace engine::text::parser
+{
+	template<math::CScalarType TValueType>
+	TValueType CharToDigit(char character)
+	{
+		return static_cast<TValueType>(character - '0');
+	}
+
+	template<math::CScalarType TValueType>
+	const char* GetValSign(const char* text, TValueType& sign)
+	{
+		sign = static_cast<TValueType>(1);
+
+		switch (*text)
+		{
+		case '-':
+			++text;
+			sign = static_cast<TValueType>(-1);
+			break;
+
+		case '+':
+			++text;
+			break;
+
+		default: break;
+		}
+
+		return text;
+	}
+
+
+	template<math::CFloatingType TValueType>
+	const char* ParseIntegralPart(const char* text, TValueType& result)
+	{
+		while (IsDigit(*text))
+		{
+			result = result * static_cast<TValueType>(10) + CharToDigit<TValueType>(*text);
+			++text;
+		}
+		return text;
+	}
+
+	template<math::CFloatingType TValueType>
+	const char* ParseDecimalPart(const char* ptr, TValueType& result)
+	{
+		TValueType   divisor = static_cast<TValueType>(1);
+
+		while (IsDigit(*ptr))
+		{
+			result = result * static_cast<TValueType>(10) + CharToDigit<TValueType>(*ptr);
+			divisor *= static_cast<TValueType>(10);
+			++ptr;
+		}
+
+		result = result / divisor;
+		return ptr;
+	}
+
+	template<math::CFloatingType TValueType>
+	const char* ParseExponent(const char* ptr, TValueType& exponent)
+	{
+		static constexpr TValueType negativePowers[] =
+		{
+			(TValueType) 1e0, (TValueType)1e-1,(TValueType)1e-2,(TValueType)1e-3,(TValueType)1e-4,
+			(TValueType)1e-5,(TValueType)1e-6,(TValueType)1e-7,(TValueType)1e-8,(TValueType)1e-9,
+			(TValueType)1e-10,(TValueType)1e-11,(TValueType)1e-12,(TValueType)1e-13,
+			(TValueType)1e-14, (TValueType)1e-15, (TValueType)1e-16, (TValueType)1e-17,(TValueType)1e-18,(TValueType)1e-19
+		};
+		static constexpr TValueType positivePowers[] =
+		{
+			(TValueType)1e0, (TValueType)1e1,(TValueType)1e2, (TValueType)1e3, (TValueType)1e4,
+			(TValueType)1e5,(TValueType)1e6,(TValueType)1e7, (TValueType)1e8,(TValueType)1e9,
+			(TValueType)1e10, (TValueType)1e11,(TValueType)1e12,(TValueType)1e13,
+			(TValueType)1e14, (TValueType)1e15,(TValueType)1e16, (TValueType)1e17,(TValueType)1e18, (TValueType)1e19
+		};
+
+		const TValueType*	powers = positivePowers;
+		int32               index = 0;
+
+		++ptr;
+		switch (*ptr)
+		{
+		case '-':
+			++ptr;
+			powers = negativePowers;
+			break;
+
+		case '+':
+			++ptr;
+			break;
+
+		default: break;
+		}
+
+		while (IsDigit(*ptr))
+		{
+			index = index * 10 + CharToDigit<int32>(*ptr);
+			++ptr;
+		}
+		exponent = (index >= 20) ? static_cast<TValueType>(0) : powers[index];
+		return ptr;
+	}
 }
--- engine/source/serialization/TextSerializer.cpp
+++ engine/source/serialization/TextSerializer.cpp
@@ -3,19 +3,19 @@
 
 namespace engine::text
 {
-	void Serialize(std::ofstream& file, const char* name, const std::string& val)
+	void Serialize(std::ostream& file, const char* name, const std::string& val)
 	{
 		const char* valName((name) ? name : "unnamed val");
 		file << "@" << "string " << val.size() << " | " << valName << '=' << val;
 	}
 
-	void Serialize(std::ofstream& file, const char* name, const char* val, uint64 length)
+	void Serialize(std::ostream& file, const char* name, const char* val, uint64 length)
 	{
 		const char* valName((name) ? name : "unnamed val");
 		file << "@" << "string " << length << " | " << valName << '=' << val;
 	}
 
-	void Deserialize(std::ifstream& file, std::string& val)
+	void Deserialize(std::istream& file, std::string& val)
 	{
 		char currentChar = static_cast<char>(file.peek());
 
@@ -48,7 +48,7 @@ namespace engine::text
 			++pos;
 	}
 
-	void MoveCursorToVal(std::ifstream& file)
+	void MoveCursorToVal(std::istream& file)
 	{
 		char currentChar = static_cast<char>(file.peek());
 
@@ -68,4 +68,84 @@ namespace engine::text
 		if (!file.eof() && currentChar != -1)
 			file.seekg(1, std::ios::cur);
 	}
+
+	const char* DeserializeString(const char* text, const char* end, std::string& val)
+	{
+		while (!parser::IsDigit(*text) && text != end)
+			++text;
+
+		uint64 size;
+		text = DeserializeInteger<uint64>(text, size);
+
+		val.resize(size);
+
+		MOVE_TEXT_CURSOR(text, end);
+		memcpy(val.data(), text, size);
+
+		return text += size;
+	}
+
+	const char* LoadFileData(std::ifstream& file, const char*& start, const char*& end)
+	{
+		file.seekg(0, std::ios::end);
+		int64 size = file.tellg();
+		file.seekg(0, std::ios::beg);
+
+		char* buf = new char[size + 1];
+
+		file.read(buf, size);
+
+		buf[size] = '\0';
+		end = buf + size;
+		start = buf;
+
+		return buf;
+	}
+
+	void UnloadFileData(const char* data)
+	{
+		delete[] data;
+	}
+
+	const char* GetValuePointer(const char* text, const char* end)
+	{
+		while (*text != '=' && text != end)
+			++text;
+
+		if (text != end)
+			++text;
+
+		return text;
+	}
+
+	const char* GetNewLine(const char* text, const char* end)
+	{
+		while (!parser::IsNewline(*text) && (text != end))
+			++text;
+
+		if (text != end)
+			++text;
+
+		return text;
+	}
 }
+
+namespace engine::text::parser
+{
+	bool IsDigit(char character)
+	{
+		return (character >= '0') && (character <= '9');
+	}
+
+	bool IsNewline(char character)
+	{
+		return ('\r' == character) || ('\n' == character);
+	}
+
+	bool IsWhitespace(char character)
+	{
+		return (character == ' ') || (character == '\t');
+	}
+
+}
+
