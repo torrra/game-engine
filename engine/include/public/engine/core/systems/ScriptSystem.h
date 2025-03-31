db46de2b4a094d1b20760101512346968d65a707
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-31 09:17:13 -0400

    Replace tabs with spaces in ScriptSystem
    Replace tabs with 4 spaces in ScriptSystem.h and ScriptSystem.cpp

--- engine/include/public/engine/core/systems/ScriptSystem.h
+++ engine/include/public/engine/core/systems/ScriptSystem.h
@@ -8,119 +8,119 @@
 
 namespace engine
 {
-	class ScriptSystem
-	{
-	private:
+    class ScriptSystem
+    {
+    private:
 
-		using FilePath = std::filesystem::path;
+        using FilePath = std::filesystem::path;
 
-	public:
+    public:
 
-		// Initialize lua state, register C functions and
-		// run engine scripts. Should be called before any
+        // Initialize lua state, register C functions and
+        // run engine scripts. Should be called before any
 
-		// Script-related function other than SetUserScriptLocation().
-		// SetUserScriptLocation should be called before this function on engine startup.
-		ENGINE_API
-		static void Startup(void);
+        // Script-related function other than SetUserScriptLocation().
+        // SetUserScriptLocation should be called before this function on engine startup.
+        ENGINE_API
+        static void Startup(void);
 
-		// Find where the engine's internal scripts
-		// are stored
-		ENGINE_API
-		static std::string FindConfigScripts(void);
-		
-		// Close lua state. Do not call any script-related
-		// functions after this
-		ENGINE_API
-		static void Shutdown(void);
+        // Find where the engine's internal scripts
+        // are stored
+        ENGINE_API
+        static std::string FindConfigScripts(void);
+        
+        // Close lua state. Do not call any script-related
+        // functions after this
+        ENGINE_API
+        static void Shutdown(void);
 
-		// Set the scripting system's active scene. This function needs to be
-		// called every time a scene is loaded, so that the scripting system
-		// interacts with the right scene graph
-		ENGINE_API
-		static void SetCurrentScene(class SceneGraph* graph);
-
-
-		// Get the current scene graph the scripting system is operating on
-		ENGINE_API
-		static SceneGraph*			GetCurrentScene(void);
-
-		// Get the engine's scripts location as a string
-		ENGINE_API
-		static const std::string&	GetConfigScriptsLocation(void);
-
-		// Register a component of a given type
-		// function: lua global function that creates a table for an existing component
-		// owner: owning entity 
-		ENGINE_API
-		static void RegisterNewComponent(const char* function, EntityHandle owner);
+        // Set the scripting system's active scene. This function needs to be
+        // called every time a scene is loaded, so that the scripting system
+        // interacts with the right scene graph
+        ENGINE_API
+        static void SetCurrentScene(class SceneGraph* graph);
 
 
-		// Add an entity to the scripting system's lua entity table
-		ENGINE_API
-		static void RegisterNewEntity(EntityHandle newEntity, const std::string& name);
+        // Get the current scene graph the scripting system is operating on
+        ENGINE_API
+        static SceneGraph*			GetCurrentScene(void);
 
-		// Add an entity's script component to the scripting system's lua script component table
-		ENGINE_API
-		static void RegisterNewScriptComponent(EntityHandle owner);
+        // Get the engine's scripts location as a string
+        ENGINE_API
+        static const std::string&	GetConfigScriptsLocation(void);
 
-		// Add a script object to an existing script component in script
-		ENGINE_API
-		static void RegisterNewScriptObject(const std::string& type, EntityHandle owner);
-
-		// Call all Start() functions in a given entity's script component
-		ENGINE_API
-		static void StartScript(EntityHandle entity);
-
-		// Call all Update() functions in a given entity's script component
-		ENGINE_API
-		static void UpdateScript(EntityHandle entity, f32 deltaTime);
-
-		// Fully shutdown and restart lua state, reset all existing lua
-		// objecs and re run all scripts
-		ENGINE_API
-		static void ResetState(class SceneGraph* newScene = nullptr);
-
-		// Set where the default user-generated scripts are stored for this project
-		ENGINE_API
-		static void SetUserScriptLocation(const char* path);
-
-		// Create a new .lua file inheriting from ScriptObject
-		ENGINE_API
-		static void CreateUserScript(const char* dirRelativePath, const char* className);
-
-		// Run all .lua files present in the directory specified as
-		// user script locations
-		ENGINE_API
-		static void RunAllUserScripts(void);
-
-		// Run a debug lua interpreter
-		// NOTE: there is no way to stop it beside killing the
-		// entire program as of now
-		// TODO: add kill condition once we have the input system ready
-		ENGINE_API
-		static void RunInterpreter(void);
-
-	private:
-
-		static void RunUserScript(const std::string& filename);
-		static void LogLuaError(void);
-
-		// Run an internal lua file
-		static void RunConfigScript(const char* script);
-
-		static std::string FormatLuaClassName(const char* name);
-
-		static ScriptSystem* GetInstance(void);
+        // Register a component of a given type
+        // function: lua global function that creates a table for an existing component
+        // owner: owning entity 
+        ENGINE_API
+        static void RegisterNewComponent(const char* function, EntityHandle owner);
 
 
-		class	SceneGraph* m_currentScene = nullptr;
-		struct	lua_State*	m_luaState = nullptr;
+        // Add an entity to the scripting system's lua entity table
+        ENGINE_API
+        static void RegisterNewEntity(EntityHandle newEntity, const std::string& name);
 
-		std::string			m_configScriptsLocation = ScriptSystem::FindConfigScripts();
-		std::string			m_userScriptsLocation;
+        // Add an entity's script component to the scripting system's lua script component table
+        ENGINE_API
+        static void RegisterNewScriptComponent(EntityHandle owner);
 
-		static std::mutex	 m_mutex;
-		static ScriptSystem* m_instance;
-	};
+        // Add a script object to an existing script component in script
+        ENGINE_API
+        static void RegisterNewScriptObject(const std::string& type, EntityHandle owner);
+
+        // Call all Start() functions in a given entity's script component
+        ENGINE_API
+        static void StartScript(EntityHandle entity);
+
+        // Call all Update() functions in a given entity's script component
+        ENGINE_API
+        static void UpdateScript(EntityHandle entity, f32 deltaTime);
+
+        // Fully shutdown and restart lua state, reset all existing lua
+        // objecs and re run all scripts
+        ENGINE_API
+        static void ResetState(class SceneGraph* newScene = nullptr);
+
+        // Set where the default user-generated scripts are stored for this project
+        ENGINE_API
+        static void SetUserScriptLocation(const char* path);
+
+        // Create a new .lua file inheriting from ScriptObject
+        ENGINE_API
+        static void CreateUserScript(const char* dirRelativePath, const char* className);
+
+        // Run all .lua files present in the directory specified as
+        // user script locations
+        ENGINE_API
+        static void RunAllUserScripts(void);
+
+        // Run a debug lua interpreter
+        // NOTE: there is no way to stop it beside killing the
+        // entire program as of now
+        // TODO: add kill condition once we have the input system ready
+        ENGINE_API
+        static void RunInterpreter(void);
+
+    private:
+
+        static void RunUserScript(const std::string& filename);
+        static void LogLuaError(void);
+
+        // Run an internal lua file
+        static void RunConfigScript(const char* script);
+
+        static std::string FormatLuaClassName(const char* name);
+
+        static ScriptSystem* GetInstance(void);
+
+
+        class	SceneGraph* m_currentScene = nullptr;
+        struct	lua_State*	m_luaState = nullptr;
+
+        std::string			m_configScriptsLocation = ScriptSystem::FindConfigScripts();
+        std::string			m_userScriptsLocation;
+
+        static std::mutex	 m_mutex;
+        static ScriptSystem* m_instance;
+    };
 }
--- engine/source/core/systems/ScriptSystem.cpp
+++ engine/source/core/systems/ScriptSystem.cpp
@@ -29,293 +29,293 @@ extern "C"
 
 namespace engine
 {
-	ScriptSystem* ScriptSystem::m_instance = nullptr;
-	std::mutex  ScriptSystem::m_mutex;
-	
-	void ScriptSystem::Startup(void)
-	{
-		GetInstance()->m_luaState = luaL_newstate();
+    ScriptSystem* ScriptSystem::m_instance = nullptr;
+    std::mutex  ScriptSystem::m_mutex;
+    
+    void ScriptSystem::Startup(void)
+    {
+        GetInstance()->m_luaState = luaL_newstate();
 
-		if (!GetInstance()->m_luaState)
-		{
-			// TODO: log error?
-			std::cout << "Unable to create Lua state\n";
-			exit(EXIT_FAILURE);			
-		}
+        if (!GetInstance()->m_luaState)
+        {
+            // TODO: log error?
+            std::cout << "Unable to create Lua state\n";
+            exit(EXIT_FAILURE);			
+        }
 
-		luaL_openlibs(GetInstance()->m_luaState);
+        luaL_openlibs(GetInstance()->m_luaState);
 
-		RegisterEntityFunctions(GetInstance()->m_luaState);
-		RegisterComponentFunctions(GetInstance()->m_luaState);
-		RegisterScriptComponentFunctions(GetInstance()->m_luaState);
-		RegisterCameraFunctions(GetInstance()->m_luaState);
-		RegisterTransformFunctions(GetInstance()->m_luaState);
-		RegisterInputFunctions(GetInstance()->m_luaState);
+        RegisterEntityFunctions(GetInstance()->m_luaState);
+        RegisterComponentFunctions(GetInstance()->m_luaState);
+        RegisterScriptComponentFunctions(GetInstance()->m_luaState);
+        RegisterCameraFunctions(GetInstance()->m_luaState);
+        RegisterTransformFunctions(GetInstance()->m_luaState);
+        RegisterInputFunctions(GetInstance()->m_luaState);
 
-		RunConfigScript("Component.lua");
-		RunConfigScript("Entity.lua");
+        RunConfigScript("Component.lua");
+        RunConfigScript("Entity.lua");
 
-		RunConfigScript("ScriptObject.lua");
-		RunConfigScript("Script.lua");
+        RunConfigScript("ScriptObject.lua");
+        RunConfigScript("Script.lua");
 
-		RunConfigScript("Camera.lua");
-		RunConfigScript("Transform.lua");
+        RunConfigScript("Camera.lua");
+        RunConfigScript("Transform.lua");
 
-		RunConfigScript("Input.lua");
+        RunConfigScript("Input.lua");
 
-		RunAllUserScripts();
-	}
+        RunAllUserScripts();
+    }
 
-	void ScriptSystem::Shutdown(void)
-	{
-		lua_close(GetInstance()->m_luaState);
-		delete m_instance;
-		m_instance = nullptr;
-	}
+    void ScriptSystem::Shutdown(void)
+    {
+        lua_close(GetInstance()->m_luaState);
+        delete m_instance;
+        m_instance = nullptr;
+    }
 
-	void ScriptSystem::SetCurrentScene(SceneGraph* graph)
-	{
-		GetInstance()->m_currentScene = graph;
-	}
+    void ScriptSystem::SetCurrentScene(SceneGraph* graph)
+    {
+        GetInstance()->m_currentScene = graph;
+    }
 
-	SceneGraph* ScriptSystem::GetCurrentScene(void)
-	{
-		return GetInstance()->m_currentScene;
-	}
+    SceneGraph* ScriptSystem::GetCurrentScene(void)
+    {
+        return GetInstance()->m_currentScene;
+    }
 
-	const std::string& ScriptSystem::GetConfigScriptsLocation(void)
-	{
-		return GetInstance()->m_configScriptsLocation;
-	}
+    const std::string& ScriptSystem::GetConfigScriptsLocation(void)
+    {
+        return GetInstance()->m_configScriptsLocation;
+    }
 
-	void ScriptSystem::RegisterNewComponent(const char* function, EntityHandle owner)
-	{
-		lua_getglobal(GetInstance()->m_luaState, function);
-		lua_pushinteger(GetInstance()->m_luaState, owner);
+    void ScriptSystem::RegisterNewComponent(const char* function, EntityHandle owner)
+    {
+        lua_getglobal(GetInstance()->m_luaState, function);
+        lua_pushinteger(GetInstance()->m_luaState, owner);
 
-		if (lua_pcall(GetInstance()->m_luaState, 1, 0, 0) != LUA_OK)
-			LogLuaError();
-	}
+        if (lua_pcall(GetInstance()->m_luaState, 1, 0, 0) != LUA_OK)
+            LogLuaError();
+    }
 
-	void ScriptSystem::RegisterNewEntity(EntityHandle newEntity, const std::string& name)
-	{
-		if (name.empty())
-			return;
+    void ScriptSystem::RegisterNewEntity(EntityHandle newEntity, const std::string& name)
+    {
+        if (name.empty())
+            return;
 
-		lua_getglobal(GetInstance()->m_luaState, "_AddEntityHandle");
-		lua_pushinteger(GetInstance()->m_luaState, newEntity);
-		lua_pushstring(GetInstance()->m_luaState, name.c_str());
+        lua_getglobal(GetInstance()->m_luaState, "_AddEntityHandle");
+        lua_pushinteger(GetInstance()->m_luaState, newEntity);
+        lua_pushstring(GetInstance()->m_luaState, name.c_str());
 
-		if (lua_pcall(GetInstance()->m_luaState, 2, 0, 0) != LUA_OK)
-			LogLuaError();
-	}
+        if (lua_pcall(GetInstance()->m_luaState, 2, 0, 0) != LUA_OK)
+            LogLuaError();
+    }
 
-	void ScriptSystem::RegisterNewScriptComponent(EntityHandle owner)
-	{
-		lua_getglobal(GetInstance()->m_luaState, "_NewScriptComponent");
-		lua_pushinteger(GetInstance()->m_luaState, owner);
-		  
-		if (lua_pcall(GetInstance()->m_luaState, 1, 0, 0) != LUA_OK)
-			LogLuaError();
-	}
+    void ScriptSystem::RegisterNewScriptComponent(EntityHandle owner)
+    {
+        lua_getglobal(GetInstance()->m_luaState, "_NewScriptComponent");
+        lua_pushinteger(GetInstance()->m_luaState, owner);
+          
+        if (lua_pcall(GetInstance()->m_luaState, 1, 0, 0) != LUA_OK)
+            LogLuaError();
+    }
 
-	void ScriptSystem::RegisterNewScriptObject(const std::string& type, EntityHandle owner)
-	{
-		lua_getglobal(GetInstance()->m_luaState, "_NewScriptObject");
-		lua_pushstring(GetInstance()->m_luaState, type.c_str());
-		lua_pushinteger(GetInstance()->m_luaState, owner);
+    void ScriptSystem::RegisterNewScriptObject(const std::string& type, EntityHandle owner)
+    {
+        lua_getglobal(GetInstance()->m_luaState, "_NewScriptObject");
+        lua_pushstring(GetInstance()->m_luaState, type.c_str());
+        lua_pushinteger(GetInstance()->m_luaState, owner);
 
-		if (lua_pcall(GetInstance()->m_luaState, 2, 0, 0) != LUA_OK)
-			LogLuaError();
-	}
+        if (lua_pcall(GetInstance()->m_luaState, 2, 0, 0) != LUA_OK)
+            LogLuaError();
+    }
 
-	std::string ScriptSystem::FindConfigScripts(void)
-	{
-		// scripts/ directory should either be in child or sibling folder
-		// (relative to the executable's working directory)
-		FilePath currentPath = std::filesystem::current_path();
-		FilePath parentPath = currentPath.parent_path();
+    std::string ScriptSystem::FindConfigScripts(void)
+    {
+        // scripts/ directory should either be in child or sibling folder
+        // (relative to the executable's working directory)
+        FilePath currentPath = std::filesystem::current_path();
+        FilePath parentPath = currentPath.parent_path();
 
-		// look for child folder
-		if (std::filesystem::is_directory(currentPath.append("scripts\\")))
-			return parentPath.string();
+        // look for child folder
+        if (std::filesystem::is_directory(currentPath.append("scripts\\")))
+            return parentPath.string();
 
-		// default to sibling folder
-		else
-			return parentPath.append("scripts\\").string();
-	}
+        // default to sibling folder
+        else
+            return parentPath.append("scripts\\").string();
+    }
 
-	void ScriptSystem::StartScript(EntityHandle entity)
-	{
-		lua_getglobal(GetInstance()->m_luaState, "_StartScript");
-		lua_pushinteger(GetInstance()->m_luaState, entity);
+    void ScriptSystem::StartScript(EntityHandle entity)
+    {
+        lua_getglobal(GetInstance()->m_luaState, "_StartScript");
+        lua_pushinteger(GetInstance()->m_luaState, entity);
 
-		if (lua_pcall(GetInstance()->m_luaState, 1, 0, 0) != LUA_OK)
-			LogLuaError();
-	}
+        if (lua_pcall(GetInstance()->m_luaState, 1, 0, 0) != LUA_OK)
+            LogLuaError();
+    }
 
-	void ScriptSystem::UpdateScript(EntityHandle entity, f32 deltaTime)
-	{
-		lua_getglobal(GetInstance()->m_luaState, "_UpdateScript");
-		lua_pushinteger(GetInstance()->m_luaState, entity);
-		lua_pushnumber(GetInstance()->m_luaState, deltaTime);
+    void ScriptSystem::UpdateScript(EntityHandle entity, f32 deltaTime)
+    {
+        lua_getglobal(GetInstance()->m_luaState, "_UpdateScript");
+        lua_pushinteger(GetInstance()->m_luaState, entity);
+        lua_pushnumber(GetInstance()->m_luaState, deltaTime);
 
-		if (lua_pcall(GetInstance()->m_luaState, 2, 0, 0) != LUA_OK)
-			LogLuaError();
-	}
+        if (lua_pcall(GetInstance()->m_luaState, 2, 0, 0) != LUA_OK)
+            LogLuaError();
+    }
 
-	void ScriptSystem::ResetState(SceneGraph* newScene)
-	{
-		SceneGraph* oldScenePtr = GetInstance()->m_currentScene;
+    void ScriptSystem::ResetState(SceneGraph* newScene)
+    {
+        SceneGraph* oldScenePtr = GetInstance()->m_currentScene;
 
-		Shutdown();
-		Startup();
+        Shutdown();
+        Startup();
 
-		if (newScene)
-			GetInstance()->m_currentScene = newScene;
-		else
-			GetInstance()->m_currentScene = oldScenePtr;
+        if (newScene)
+            GetInstance()->m_currentScene = newScene;
+        else
+            GetInstance()->m_currentScene = oldScenePtr;
 
-		GetInstance()->m_currentScene->RegisterAllComponents();
-	}
+        GetInstance()->m_currentScene->RegisterAllComponents();
+    }
 
-	void ScriptSystem::SetUserScriptLocation(const char* path)
-	{
-		GetInstance()->m_userScriptsLocation = path;
-	}
+    void ScriptSystem::SetUserScriptLocation(const char* path)
+    {
+        GetInstance()->m_userScriptsLocation = path;
+    }
 
-	void ScriptSystem::CreateUserScript(const char* dirRelativePath, const char* className)
-	{
-		std::string fullPath = GetInstance()->m_userScriptsLocation +
-							  ((dirRelativePath) ? dirRelativePath : "") +
-							  ((className) ? className : "UserScript") + ".lua";
+    void ScriptSystem::CreateUserScript(const char* dirRelativePath, const char* className)
+    {
+        std::string fullPath = GetInstance()->m_userScriptsLocation +
+                              ((dirRelativePath) ? dirRelativePath : "") +
+                              ((className) ? className : "UserScript") + ".lua";
 
-		if (std::filesystem::exists(fullPath))
-			return;
+        if (std::filesystem::exists(fullPath))
+            return;
 
-		std::string formattedName = FormatLuaClassName(className);
+        std::string formattedName = FormatLuaClassName(className);
 
-		if (formattedName.empty())
-			return;
+        if (formattedName.empty())
+            return;
 
-		std::ofstream newFile(fullPath,
-							 std::ios::out);
+        std::ofstream newFile(fullPath,
+                             std::ios::out);
 
-		if (!newFile)
-			return;
+        if (!newFile)
+            return;
 
-		const char fileData[] = "%s = ScriptObject:_new()\n\n-- Define member variables \
+        const char fileData[] = "%s = ScriptObject:_new()\n\n-- Define member variables \
 here\n\n-- Is executed once when the object becomes active\nfunction %s:Start()\n\nend\n\n\n\
 -- Is executed every tick\nfunction %s:Update(deltaTime)\n\nend\n\n\n-- Engine definitions\n\
 ScriptObjectTypes.%s = %s\nreturn %s";
 
-		char textBuffer[1024];
+        char textBuffer[1024];
 
-		sprintf_s(textBuffer, fileData, className, className, className,
-									  formattedName.c_str(), className, className);
+        sprintf_s(textBuffer, fileData, className, className, className,
+                                      formattedName.c_str(), className, className);
 
-		newFile << textBuffer;
-		newFile.close();
-		
-		FilePath pathObject(fullPath);
-		
-		// Open new file in text editor
-		ShellExecuteA(NULL, NULL, std::filesystem::absolute(fullPath).string().c_str(), NULL, NULL, SW_SHOW);
+        newFile << textBuffer;
+        newFile.close();
+        
+        FilePath pathObject(fullPath);
+        
+        // Open new file in text editor
+        ShellExecuteA(NULL, NULL, std::filesystem::absolute(fullPath).string().c_str(), NULL, NULL, SW_SHOW);
 
-		// Run lua file to register existing type
-		RunUserScript(fullPath);
-	}
+        // Run lua file to register existing type
+        RunUserScript(fullPath);
+    }
 
-	void ScriptSystem::RunInterpreter(void)
-	{
-		char buff[256];
+    void ScriptSystem::RunInterpreter(void)
+    {
+        char buff[256];
 
-		while (fgets(buff, sizeof(buff), stdin) != NULL)
-		{		
-			if (luaL_dostring(GetInstance()->m_luaState, buff) != LUA_OK)
-				LogLuaError();
-		}
-	}
+        while (fgets(buff, sizeof(buff), stdin) != NULL)
+        {		
+            if (luaL_dostring(GetInstance()->m_luaState, buff) != LUA_OK)
+                LogLuaError();
+        }
+    }
 
-	void ScriptSystem::LogLuaError(void)
-	{
-		std::cout << lua_tostring(GetInstance()->m_luaState, -1) << '\n';
-		lua_pop(GetInstance()->m_luaState, 1);
-	}
+    void ScriptSystem::LogLuaError(void)
+    {
+        std::cout << lua_tostring(GetInstance()->m_luaState, -1) << '\n';
+        lua_pop(GetInstance()->m_luaState, 1);
+    }
 
-	void ScriptSystem::RunConfigScript(const char* script)
-	{
-		std::string path = ScriptSystem::GetConfigScriptsLocation() + script;
+    void ScriptSystem::RunConfigScript(const char* script)
+    {
+        std::string path = ScriptSystem::GetConfigScriptsLocation() + script;
 
-		if (luaL_dofile(GetInstance()->m_luaState, path.c_str()) != LUA_OK)
-			LogLuaError();
-	}
+        if (luaL_dofile(GetInstance()->m_luaState, path.c_str()) != LUA_OK)
+            LogLuaError();
+    }
 
-	std::string ScriptSystem::FormatLuaClassName(const char* name)
-	{
-		constexpr uint64 maxClassNameLength = 64;
-		std::string formattedName = name;
+    std::string ScriptSystem::FormatLuaClassName(const char* name)
+    {
+        constexpr uint64 maxClassNameLength = 64;
+        std::string formattedName = name;
 
 
-		if (formattedName.size() > maxClassNameLength)
-			return std::string();
+        if (formattedName.size() > maxClassNameLength)
+            return std::string();
 
-		for (char& character : formattedName)
-		{
-			if ((!isalnum(character)) && (character != '_'))
-				return std::string();
+        for (char& character : formattedName)
+        {
+            if ((!isalnum(character)) && (character != '_'))
+                return std::string();
 
-			if (character >= 'A' && character <= 'Z')
-				character += 32;
-		}
+            if (character >= 'A' && character <= 'Z')
+                character += 32;
+        }
 
-		return formattedName;
-	}
+        return formattedName;
+    }
 
-	ScriptSystem* ScriptSystem::GetInstance(void)
-	{
-		if (!m_instance)
-		{
-			std::unique_lock lock(m_mutex);
+    ScriptSystem* ScriptSystem::GetInstance(void)
+    {
+        if (!m_instance)
+        {
+            std::unique_lock lock(m_mutex);
 
-			if (!m_instance)
-				m_instance = new ScriptSystem();
+            if (!m_instance)
+                m_instance = new ScriptSystem();
 
-		}
+        }
 
-		return m_instance;
-	}
+        return m_instance;
+    }
 
-	void ScriptSystem::RunUserScript(const std::string& filename)
-	{
-		if (luaL_dofile(GetInstance()->m_luaState, filename.c_str()) != LUA_OK)
-			LogLuaError();
-	}
+    void ScriptSystem::RunUserScript(const std::string& filename)
+    {
+        if (luaL_dofile(GetInstance()->m_luaState, filename.c_str()) != LUA_OK)
+            LogLuaError();
+    }
 
-	void ScriptSystem::RunAllUserScripts(void)
-	{	
-		if (!std::filesystem::exists(GetInstance()->m_userScriptsLocation))
-			return;
+    void ScriptSystem::RunAllUserScripts(void)
+    {	
+        if (!std::filesystem::exists(GetInstance()->m_userScriptsLocation))
+            return;
 
-		for (auto& file : std::filesystem::recursive_directory_iterator(GetInstance()->m_userScriptsLocation))
-		{
-			if (!file.exists())
-				continue;
+        for (auto& file : std::filesystem::recursive_directory_iterator(GetInstance()->m_userScriptsLocation))
+        {
+            if (!file.exists())
+                continue;
 
-			if (!file.is_regular_file())
-				continue;
+            if (!file.is_regular_file())
+                continue;
 
-			std::string filename = file.path().string();
-			uint64 filenameLength = filename.size();
+            std::string filename = file.path().string();
+            uint64 filenameLength = filename.size();
 
-			// look for ".lua" extension ans skip if not found
-			if (filenameLength <= 4 || memcmp(filename.data() + filenameLength - 4, ".lua", 4))
-				continue;
+            // look for ".lua" extension ans skip if not found
+            if (filenameLength <= 4 || memcmp(filename.data() + filenameLength - 4, ".lua", 4))
+                continue;
 
-			std::cout << "Running file: " << filename << '\n';
-			RunUserScript(filename);
-		}	
-	}
+            std::cout << "Running file: " << filename << '\n';
+            RunUserScript(filename);
+        }	
+    }
 
 
 }
