964877d73418fec3adf57fa172cefc96935d6763
Justine <jus.martin@student.isartdigital.com>
2025-03-31 15:01:37 -0400

    Test the debug draw implementation

--- editor/source/Main.cpp
+++ editor/source/Main.cpp
@@ -42,10 +42,10 @@ int main(void)
 		project.StartUp(engine);
 
         /// ---------------- Create entity ---------------- 
-        engine::EntityHandle object = engine.GetGraph()->CreateEntity("First");
-        engine.GetGraph()->CreateComponent<engine::Transform>(object)->SetTransform(
-            math::Vector3f(0.f, 2.f, 0.f),
-            math::Quatf(1.f, 0.f, 0.f, 0.f));
+        //engine::EntityHandle object = engine.GetGraph()->CreateEntity("First");
+        //engine.GetGraph()->CreateComponent<engine::Transform>(object)->SetTransform(
+        //    math::Vector3f(0.f, 2.f, 0.f),
+        //    math::Quatf(1.f, 0.f, 0.f, 0.f));
         engine::EntityHandle floor = engine.GetGraph()->CreateEntity("Floor");
 
         /// ---------------- PhysicsEngine use ---------------- 
@@ -62,22 +62,25 @@ int main(void)
         engine::RigidBodyDynamic* rigidBody = new engine::RigidBodyDynamic(engine.GetGraph()->GetEntity("Padoru")->GetHandle(), engine.GetGraph());
         rigidBody->CreateDynamicRigidBody(engine::PhysicsEngine::Get(), *material, engine::CAPSULE);
 
-        engine::RigidBodyDynamic* first = new engine::RigidBodyDynamic(object, engine.GetGraph());
-        first->SetGravityDisabled(true);
-        first->CreateDynamicSphereRigidBody(engine::PhysicsEngine::Get(), *material, 1.f);
+        //engine::RigidBodyDynamic* first = new engine::RigidBodyDynamic(object, engine.GetGraph());
+        //first->SetGravityDisabled(true);
+        //first->CreateDynamicSphereRigidBody(engine::PhysicsEngine::Get(), *material, 1.f);
         engine::RigidBodyStatic* floorRigidBody = new engine::RigidBodyStatic(floor, engine.GetGraph());
         floorRigidBody->CreatePlaneStaticRigidBody(engine::PhysicsEngine::Get(), *floorMaterial);
 
-        engine::Camera* camera = engine.GetGraph()->GetComponent<engine::Camera>(engine.GetGraph()->GetEntity("Camera")->GetHandle());
-        math::Matrix4f projViewMatrix = camera->ViewProjection();
+
+            engine::Camera* camera = engine.GetGraph()->GetComponent<engine::Camera>(engine.GetGraph()->GetEntity("Camera")->GetHandle());
 
 		while (!engine.GetWindow()->ShouldWindowClose())
 		{
+            math::Matrix4f projViewMatrix = camera->ViewProjection();
 			project.Update(engine);
 			engine.Update();
             engine::PhysicsEngine::Get().StepSimulation(1.f / 60.f, &projViewMatrix);
             rigidBody->UpdateEntity(engine.GetGraph()->GetEntity("Padoru")->GetHandle());
             rigidBody->UpdateRigidBody(*engine.GetGraph()->GetComponent<engine::Transform>(engine.GetGraph()->GetEntity("Padoru")->GetHandle()));
+
+            engine.GetWindow()->UpdateBuffers();
 		}
 
         /// ---------------- Clean ---------------- 
