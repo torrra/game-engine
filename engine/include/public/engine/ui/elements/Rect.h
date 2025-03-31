c335144f01aaa6fd65dbb6eeef6e18bb6b966074
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-31 11:06:16 -0400

    Replace tabs with spaces in ui/elements
    Replace tabs with 4 spaces in ui/elements include and source
    directories
    Button.h/.cpp
    Image.h/.cpp
    Label.h/.cpp
    ProgressBar.h/.cpp
    Rext.h/.cpp

--- engine/include/public/engine/ui/elements/Rect.h
+++ engine/include/public/engine/ui/elements/Rect.h
@@ -8,21 +8,21 @@
 
 namespace engine
 {
-	// UI rectangle shapes
-	class Rectangle : public UIElement
-	{
-	public:
-		ENGINE_API			Rectangle(void) = delete;
-		ENGINE_API			Rectangle(math::Vector2f const& position, math::Vector2f const& size);
-		ENGINE_API virtual ~Rectangle(void) override = default;
+    // UI rectangle shapes
+    class Rectangle : public UIElement
+    {
+    public:
+        ENGINE_API			Rectangle(void) = delete;
+        ENGINE_API			Rectangle(math::Vector2f const& position, math::Vector2f const& size);
+        ENGINE_API virtual ~Rectangle(void) override = default;
 
-		virtual void		Render(void) override;
+        virtual void		Render(void) override;
 
-		ENGINE_API void		SetColor(f32 red, f32 green, f32 blue, f32 alpha = 1.0f);
-		ENGINE_API void		SetRounding(f32 rounding);
+        ENGINE_API void		SetColor(f32 red, f32 green, f32 blue, f32 alpha = 1.0f);
+        ENGINE_API void		SetRounding(f32 rounding);
 
-	private:
-		f32		m_rounding;
-		uint32	m_color;
-	};
+    private:
+        f32		m_rounding;
+        uint32	m_color;
+    };
 }
