b1ab583eaca030b5b0c6061fe98c7a72e6310783
Justine <jus.martin@student.isartdigital.com>
2025-03-31 15:51:54 -0400

    Implement internal open gl error debug

---
+++ engine/include/private/InternalOpenGLError.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#pragma region GLAD/GLFW
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#pragma endregion
+
+#pragma region Standard
+
+#include <iostream>
+
+#pragma endregion
+
+namespace engine
+{
+    void OpenGLError(void);
+    void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, GLchar const* message, void const* user_param);
+
+} // !Namespace engine
+inline void engine::OpenGLError(void)
+{
+    auto error = glGetError();
+    if (error)
+    {
+        glEnable(GL_DEBUG_OUTPUT);
+        glDebugMessageCallback(message_callback, nullptr);
+        __debugbreak();
+    }
+}
+
+inline void engine::message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, GLchar const* message, void const* user_param)
+{
+    user_param;
+    length;
+    auto const src_str = [source]() {
+        switch (source)
+        {
+        case GL_DEBUG_SOURCE_API: return "API";
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM: return "WINDOW SYSTEM";
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: return "SHADER COMPILER";
+        case GL_DEBUG_SOURCE_THIRD_PARTY: return "THIRD PARTY";
+        case GL_DEBUG_SOURCE_APPLICATION: return "APPLICATION";
+        case GL_DEBUG_SOURCE_OTHER: return "OTHER";
+        default: return "NONE";
+        }
+        }();
+
+    auto const type_str = [type]() {
+        switch (type)
+        {
+        case GL_DEBUG_TYPE_ERROR: return "ERROR";
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
+        case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
+        case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
+        case GL_DEBUG_TYPE_MARKER: return "MARKER";
+        case GL_DEBUG_TYPE_OTHER: return "OTHER";
+        default: return "NONE";
+        }
+        }();
+
+    auto const severity_str = [severity]() {
+        switch (severity)
+        {
+        case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
+        case GL_DEBUG_SEVERITY_LOW: return "LOW";
+        case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
+        case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
+        default: return "NONE";
+        }
+        }();
+    printf("%s, %s, %s, %d, %s\n", src_str, type_str, severity_str, (int)id, message);
+}
--- engine/source/physics/InternalPhysxDebugDraw.cpp
+++ engine/source/physics/InternalPhysxDebugDraw.cpp
@@ -4,6 +4,7 @@
 
 #include "physics/InternalPhysXStruct.hpp"
 #include "physics/InternalPhyxConversion.hpp"
+#include "InternalOpenGLError.hpp"
 
 #pragma endregion
 
@@ -98,48 +99,6 @@ void engine::DebugDraw::RenderDebugDraw(math::Matrix4f* inProjViewMatrix, uint32
     //    glDebugMessageCallback(message_callback, nullptr);
     //    __debugbreak();
     //}
+    OpenGLError();
 }
 
-void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, GLchar const* message, void const* user_param)
-{
-    user_param;
-    length;
-    auto const src_str = [source]() {
-        switch (source)
-        {
-        case GL_DEBUG_SOURCE_API: return "API";
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM: return "WINDOW SYSTEM";
-        case GL_DEBUG_SOURCE_SHADER_COMPILER: return "SHADER COMPILER";
-        case GL_DEBUG_SOURCE_THIRD_PARTY: return "THIRD PARTY";
-        case GL_DEBUG_SOURCE_APPLICATION: return "APPLICATION";
-        case GL_DEBUG_SOURCE_OTHER: return "OTHER";
-        default: return "NONE";
-        }
-        }();
-
-    auto const type_str = [type]() {
-        switch (type)
-        {
-        case GL_DEBUG_TYPE_ERROR: return "ERROR";
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
-        case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
-        case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
-        case GL_DEBUG_TYPE_MARKER: return "MARKER";
-        case GL_DEBUG_TYPE_OTHER: return "OTHER";
-        default: return "NONE";
-        }
-        }();
-
-    auto const severity_str = [severity]() {
-        switch (severity)
-        {
-        case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
-        case GL_DEBUG_SEVERITY_LOW: return "LOW";
-        case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
-        case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
-        default: return "NONE";
-        }
-        }();
-    printf("%s, %s, %s, %d, %s\n", src_str, type_str, severity_str, (int)id, message);
-}
