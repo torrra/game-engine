de2bc1b5b699bc54e70f5b8c31ef0f57ea186d64
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-31 09:16:25 -0400

    Replace tabs with spaces in core dirs
    Replace tabs with 4 spaces in core directories
    Component.h/.cpp
    Entity.h/.cpp
    SceneGraph.h/.cpp
    TyoesECS.h

--- engine/include/public/engine/core/Component.h
+++ engine/include/public/engine/core/Component.h
@@ -8,68 +8,68 @@
 
 namespace engine
 {
-	class Component
-	{
-	private:
+    class Component
+    {
+    private:
 
-		friend class SceneGraph;
+        friend class SceneGraph;
 
-	public:
+    public:
 
-		ENGINE_API Component(void) = default;
-		ENGINE_API Component(EntityHandle owner, class SceneGraph* scene);
-		ENGINE_API virtual ~Component(void) = default;
+        ENGINE_API Component(void) = default;
+        ENGINE_API Component(EntityHandle owner, class SceneGraph* scene);
+        ENGINE_API virtual ~Component(void) = default;
 
-		virtual void Register(void) = 0;
+        virtual void Register(void) = 0;
 
-		// Is this component a valid object?
-		// true: this object is a valid component instance
-		// false: this object is 'dead' and subject to overwrite
-		ENGINE_API
-		bool IsValid(void) const;
+        // Is this component a valid object?
+        // true: this object is a valid component instance
+        // false: this object is 'dead' and subject to overwrite
+        ENGINE_API
+        bool IsValid(void) const;
 
-		// Is this component included when updating the scene?
-		ENGINE_API
-		bool IsActive(void) const;
+        // Is this component included when updating the scene?
+        ENGINE_API
+        bool IsActive(void) const;
 
-		// Set the component's active status
-		// true: will be included in update
-		// false: will be excluded in update
-		ENGINE_API
-		void Activate(bool activeState);
+        // Set the component's active status
+        // true: will be included in update
+        // false: will be excluded in update
+        ENGINE_API
+        void Activate(bool activeState);
 
-		// Permanently set object up for destruction
-		ENGINE_API
-		void Invalidate(void);
+        // Permanently set object up for destruction
+        ENGINE_API
+        void Invalidate(void);
 
-	protected:
+    protected:
 
-		// which entity in scene graph owns this component
-		EntityHandle	m_owner = static_cast<EntityHandle>(-1);
+        // which entity in scene graph owns this component
+        EntityHandle	m_owner = static_cast<EntityHandle>(-1);
 
-		// Scene graph in which ths component exists
-		class SceneGraph* m_currentScene = nullptr;
+        // Scene graph in which ths component exists
+        class SceneGraph* m_currentScene = nullptr;
 
-		// status modifiers that affect behavior (inactive, invalid)
-		uint64			m_flags = 0;
-	};
+        // status modifiers that affect behavior (inactive, invalid)
+        uint64			m_flags = 0;
+    };
 
 
-	// Defines whether a type has objects that NEED to be updated after their parents
-	// e.g. a transform component should be updated after its owner's transform.
-	// False by default, must be manually instantiated and defined as true if needed
-	template <CValidComponent TComponentType>
-	struct UpdateAfterParent
-	{
-		static constexpr bool m_value = false;
-	};
+    // Defines whether a type has objects that NEED to be updated after their parents
+    // e.g. a transform component should be updated after its owner's transform.
+    // False by default, must be manually instantiated and defined as true if needed
+    template <CValidComponent TComponentType>
+    struct UpdateAfterParent
+    {
+        static constexpr bool m_value = false;
+    };
 
-	// Defines if a component type should be updated each tick in the main loop
-	// False by default, must be manually instantiated and defined as true if needed
-	template <CValidComponent TComponentType>
-	struct UpdateComponent
-	{
-		static constexpr bool m_value = false;
-	};
+    // Defines if a component type should be updated each tick in the main loop
+    // False by default, must be manually instantiated and defined as true if needed
+    template <CValidComponent TComponentType>
+    struct UpdateComponent
+    {
+        static constexpr bool m_value = false;
+    };
 
 }
--- engine/include/public/engine/core/TypesECS.h
+++ engine/include/public/engine/core/TypesECS.h
@@ -5,37 +5,37 @@
 namespace engine
 {
 
-	namespace ecs
-	{
-		// Define special cases that may affect behavior
-		enum EFlags : uint64
-		{
-			NONE = 0,
+    namespace ecs
+    {
+        // Define special cases that may affect behavior
+        enum EFlags : uint64
+        {
+            NONE = 0,
 
-			// Object is dead and can be written over
-			INVALID_OBJECT = 1,
+            // Object is dead and can be written over
+            INVALID_OBJECT = 1,
 
-			// Object will not be updated
-			INACTIVE_OBJECT = (uint64)(1 << 1)
-		};
-	}
+            // Object will not be updated
+            INACTIVE_OBJECT = (uint64)(1 << 1)
+        };
+    }
 
 
-	// Unique Entity identifier, made up of 32 index bits (lower) and 32 UID bits (higher)
-	// The index bits refer to the entity's position in memory, and the UID is meant to
-	// differentiate between unique entities, as two entities could have the same memory
-	// locations at different points in time.
-	using EntityHandle = int64;
+    // Unique Entity identifier, made up of 32 index bits (lower) and 32 UID bits (higher)
+    // The index bits refer to the entity's position in memory, and the UID is meant to
+    // differentiate between unique entities, as two entities could have the same memory
+    // locations at different points in time.
+    using EntityHandle = int64;
 
-	// Component abstract class. All component types must inherit from it to be
-	// considerred valid components
-	class Component;
+    // Component abstract class. All component types must inherit from it to be
+    // considerred valid components
+    class Component;
 
-	// Entity class. Represent game objects
-	class Entity;
+    // Entity class. Represent game objects
+    class Entity;
 
-	// Ensure a component type inherits from Component class
-	template <typename TComponentType>
-	concept CValidComponent = std::derived_from<TComponentType, Component>;
+    // Ensure a component type inherits from Component class
+    template <typename TComponentType>
+    concept CValidComponent = std::derived_from<TComponentType, Component>;
 
 }
