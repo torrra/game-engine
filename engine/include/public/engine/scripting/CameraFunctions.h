5f2d83c871f5ce3cf32769844728dd47407c3aaf
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-03-31 10:53:32 -0400

    Replace tabs with spaces in scripting dir
    Replace tabs with 4 spaces in scripting include and
    source dirs

--- engine/include/public/engine/scripting/CameraFunctions.h
+++ engine/include/public/engine/scripting/CameraFunctions.h
@@ -2,27 +2,27 @@
 
 extern "C"
 {
-	int script_GetCameraRef(struct lua_State* luaState);
-	
-	int script_MoveCamera(struct lua_State* luaState);
-	int script_RotateCameraEulers(struct lua_State* luaState);
+    int script_GetCameraRef(struct lua_State* luaState);
+    
+    int script_MoveCamera(struct lua_State* luaState);
+    int script_RotateCameraEulers(struct lua_State* luaState);
 
-	int script_GetCameraPosition(struct lua_State* luaState);
-	int script_GetCameraRotation(struct lua_State* luaState);
+    int script_GetCameraPosition(struct lua_State* luaState);
+    int script_GetCameraRotation(struct lua_State* luaState);
 
-	int script_GetCameraFOV(struct lua_State* luaState);
-	int script_GetCameraNearPlane(struct lua_State* luaState);
-	int script_GetCameraFarPlace(struct lua_State* luaState);
+    int script_GetCameraFOV(struct lua_State* luaState);
+    int script_GetCameraNearPlane(struct lua_State* luaState);
+    int script_GetCameraFarPlace(struct lua_State* luaState);
 
-	int script_SetCameraPosition(struct lua_State* luaState);
-	int script_SetCameraRotation(struct lua_State* luaState);
+    int script_SetCameraPosition(struct lua_State* luaState);
+    int script_SetCameraRotation(struct lua_State* luaState);
 
-	int script_SetCameraFOV(struct lua_State* luaState);
-	int script_SetCameraNearPlace(struct lua_State* luaState);
-	int script_SetCameraFarPlane(struct lua_State* luaState);
+    int script_SetCameraFOV(struct lua_State* luaState);
+    int script_SetCameraNearPlace(struct lua_State* luaState);
+    int script_SetCameraFarPlane(struct lua_State* luaState);
 }
 
 namespace engine
 {
-	void RegisterCameraFunctions(struct lua_State* luaState);
+    void RegisterCameraFunctions(struct lua_State* luaState);
 }
