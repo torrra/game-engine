3cc137696b41d776faacb8d2f48e15bc9d120062
Marc Levesque <m.levesque@student.isartdigital.com>
2025-04-01 13:42:20 -0400

    Change to spacing & line ending
    Set spacing to use spaces & line ending to CRLF

--- engine/source/viewport/Viewport.cpp
+++ engine/source/viewport/Viewport.cpp
@@ -11,59 +11,59 @@
 #include <imgui/imgui.h>
 
 engine::Viewport::Viewport(const char* title)
-	: m_title(title), m_bgColor(0.1f, 0.1f, 0.1f, 0.1f)
+    : m_title(title), m_bgColor(0.1f, 0.1f, 0.1f, 0.1f)
 {}
 
 engine::Viewport::Viewport(const char* title, math::Vector4f bgColor)
-	: m_title(title), m_bgColor(bgColor)
+    : m_title(title), m_bgColor(bgColor)
 {}
 
 void engine::Viewport::DrawViewport(void)
 {
-	// Window flags
-	static const ImGuiWindowFlags flags =
-		ImGuiWindowFlags_NoCollapse |
-		ImGuiWindowFlags_NoScrollbar |
-		ImGuiWindowFlags_NoScrollWithMouse |
-		ImGuiWindowFlags_NoBackground |
-		ImGuiWindowFlags_NoSavedSettings;
+    // Window flags
+    static const ImGuiWindowFlags flags =
+        ImGuiWindowFlags_NoCollapse |
+        ImGuiWindowFlags_NoScrollbar |
+        ImGuiWindowFlags_NoScrollWithMouse |
+        ImGuiWindowFlags_NoBackground |
+        ImGuiWindowFlags_NoSavedSettings;
 
-	
-	// TODO: add default docking node
-	ImGui::SetNextWindowSize({960, 540}, ImGuiCond_Once);
-	ImGui::Begin(m_title.c_str(), nullptr, flags);
+    
+    // TODO: add default docking node
+    ImGui::SetNextWindowSize({960, 540}, ImGuiCond_Once);
+    ImGui::Begin(m_title.c_str(), nullptr, flags);
 
-	// Transform
-	math::Vector2f position = ImGui::GetCursorScreenPos();
-	math::Vector2f regionAvail = ImGui::GetContentRegionAvail();
-	math::Vector2i sizePx
-	(
-		static_cast<int32>(regionAvail.GetX()),
-		static_cast<int32>(regionAvail.GetY())
-	);
+    // Transform
+    math::Vector2f position = ImGui::GetCursorScreenPos();
+    math::Vector2f regionAvail = ImGui::GetContentRegionAvail();
+    math::Vector2i sizePx
+    (
+        static_cast<int32>(regionAvail.GetX()),
+        static_cast<int32>(regionAvail.GetY())
+    );
 
-	glViewport(0, 0, sizePx.GetX(), sizePx.GetY());
-	m_fbo.RescaleFBO(sizePx.GetX(), sizePx.GetY());
+    glViewport(0, 0, sizePx.GetX(), sizePx.GetY());
+    m_fbo.RescaleFBO(sizePx.GetX(), sizePx.GetY());
 
-	ImGui::Image(
-		m_fbo.GetFrameTexture(),
-		regionAvail,
-		math::Vector2f(0.F, 1.F),
-		math::Vector2f(1.F, 0.F)
-	);
-	
-	ImGui::End();
+    ImGui::Image(
+        m_fbo.GetFrameTexture(),
+        regionAvail,
+        math::Vector2f(0.F, 1.F),
+        math::Vector2f(1.F, 0.F)
+    );
+    
+    ImGui::End();
 }
 
 void engine::Viewport::RenderToViewport(SceneGraph* sceneGraph)
 {
-	m_fbo.Bind();
-	wnd::ClearWindow(m_bgColor[0], m_bgColor[1], m_bgColor[2], m_bgColor[3]);
-	ThreadManager::RenderScene(sceneGraph);
-	m_fbo.Unbind();
+    m_fbo.Bind();
+    wnd::ClearWindow(m_bgColor[0], m_bgColor[1], m_bgColor[2], m_bgColor[3]);
+    ThreadManager::RenderScene(sceneGraph);
+    m_fbo.Unbind();
 }
 
 void engine::Viewport::SetBgColor(math::Vector4f const& bgColor)
 {
-	m_bgColor = bgColor;
+    m_bgColor = bgColor;
 }
