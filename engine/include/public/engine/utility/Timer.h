66ba8c0e59b7e503e9e33bbf2f8bba62be0b7f5a
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-04-14 13:39:48 -0400

    Add missing entity component flags
    Add CAMERA and RIGIDBODY_STATIC component flags in Entity.h

--- engine/include/public/engine/utility/Timer.h
+++ engine/include/public/engine/utility/Timer.h
@@ -12,9 +12,10 @@ namespace engine
         ENGINE_API ~Time(void) = default;
 
         ENGINE_API void Update();
+        ENGINE_API void Reset(void);
 
-        ENGINE_API f32 GetDeltaTime(void);
-        ENGINE_API f32 GetTotalTime(void);
+        ENGINE_API f32 GetDeltaTime(void) const;
+        ENGINE_API f32 GetTotalTime(void) const;
     
     private:
         f32 m_deltaTime;
