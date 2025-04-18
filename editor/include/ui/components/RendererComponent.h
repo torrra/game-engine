54ee185dccf624635168cc21fe50a85f565bad79
Marc Levesque <m.levesque@student.isartdigital.com>
2025-04-18 12:45:53 -0400

    Add contents to render component UI
    Add temporary content

--- editor/include/ui/components/RendererComponent.h
+++ editor/include/ui/components/RendererComponent.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ui/components/Component.h"
-
+#include <engine/core/components/Renderer.h>
 #include <engine/CoreTypes.h>
 
 namespace editor
@@ -12,9 +12,11 @@ namespace editor
         RendererComponent(void);
         ~RendererComponent(void);
 
+        void SetRenderer(engine::Renderer* renderer);
     protected:
         virtual void SectionContent(void) override;
 
     private:
+        engine::Renderer* m_renderer;
     };
 }
--- editor/source/ui/components/RendererComponent.cpp
+++ editor/source/ui/components/RendererComponent.cpp
@@ -3,14 +3,23 @@
 #include <engine/ui/UIComponent.h>
 
 editor::RendererComponent::RendererComponent(void)
+    : m_renderer(nullptr)
 {
     SetName("Renderer");
 }
 
 editor::RendererComponent::~RendererComponent(void)
-{}
+{
+    m_renderer = nullptr;
+}
+
+void editor::RendererComponent::SetRenderer(engine::Renderer* renderer)
+{
+    m_renderer = renderer;
+}
 
 void editor::RendererComponent::SectionContent(void)
 {
-    ui::Text("e");
+    ui::Text("Model: to do"); // TODO: add input for model (need asset window)
+    ui::Text("Shader: to do"); // TODO: add input for shader (need asset window)
 }
