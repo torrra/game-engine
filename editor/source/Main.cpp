42947c959449c7a973eab538fae945cb3074c8a2
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-04-22 09:38:11 -0400

    Add Application tests in main
    Add EditorApplication tests to Main.cpp

--- editor/source/Main.cpp
+++ editor/source/Main.cpp
@@ -11,12 +11,13 @@
 #include "engine/core/SceneGraph.h"
 #include "engine/core/Entity.h"
 #include "engine/core/components/Transform.h"
-#include <engine/input/Input.h>
 #include "engine/resource/ResourceManager.h"
+#include <engine/input/Input.h>
+
 #include <engine/resource/texture/Texture.h>
 #include <engine/core/systems/ScriptSystem.h>
 #include "engine/resource/model/Model.h"
-
+#include "ui/EditorApplication.h"
 #include "Example.h"
 
 #include "engine/ConsoleLog.hpp"
@@ -28,7 +29,7 @@
 #include "ui/Viewport.h"
 
 // HACK: ductape style solution to re-purpose main
-#define TESTING_PHYSX 0
+#define TESTING_PHYSX 1
 
 // Use dedicated graphics card
 extern "C" 
@@ -43,7 +44,10 @@ int main(void)
 	// Memory check
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	{
+		//engine::Engine engine(false);
+
 		engine::Engine engine;
+        engine.SetEditorApplication(new editor::EditorApplication("Editor"));
 		
 		if (engine.Startup("Editor", nullptr))
 			return -1;
@@ -75,6 +79,8 @@ int main(void)
 
         engine::Camera* camera = engine.GetGraph()->GetComponent<engine::Camera>(engine.GetGraph()->GetEntity("Camera")->GetHandle());
 
+        engine.m_activeScene.Start();
+
 		while (!engine.GetWindow()->ShouldWindowClose())
 		{
             math::Matrix4f projViewMatrix = camera->ViewProjection();
@@ -83,13 +89,13 @@ int main(void)
             // Update the entity in regard to the rigid body (gravity for example)
             rb->UpdateEntity();
 			project.Update(engine);
-			engine.Update();
+            engine.UpdateGameplay();
             // Update the debug draw
-            engine::PhysicsEngine::Get().UpdateDebugDraw(&projViewMatrix);
+           // engine::PhysicsEngine::Get().UpdateDebugDraw(&projViewMatrix);
             // Update the rigid body in regard to the entity (movement by keyboard input for example)
             rb->UpdateRigidBody();
 
-            engine.PostUpdate();
+            engine.UpdateApplicationWindow();
 		}
 
         /// ---------------- Clean ---------------- 
@@ -116,89 +122,102 @@ int main(void)
 #define SHADER_PROGRAM_VERTEX	".\\shaders\\ModelTextured.vert"
 #define SHADER_PROGRAM_FRAGMENT ".\\shaders\\ModelTextured.frag"
 
+int appMain(void)
+{
+    engine::Engine engine(false);
+    //engine.SetEditorApplication(new editor::EditorApplication("Editor"));
+
+    if (engine.Startup("Editor", nullptr))
+    {
+        engine.ShutDown();
+        return -1;
+    }
+
+    // All the stuff in the following region is temporary & for testing only
+#pragma region LoadTemporaryStuff
+        // Enable the following keys
+    int32 keys[] =
+    { KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_W, KEY_S, KEY_A, KEY_D, KEY_ESCAPE };
+
+    for (int32 key : keys)
+        engine::Input::RegisterInput(key);
+    engine::ResourceManager::Load<engine::Model>(MODEL_FILE);
+    engine::ResourceManager::Load<engine::Texture>(TEXTURE_FILE);
+
+    engine::ResourceManager::LoadShader(
+        SHADER_PROGRAM_NAME,
+        SHADER_PROGRAM_VERTEX,
+        SHADER_PROGRAM_FRAGMENT
+    );
+
+    engine::ScriptSystem::CreateUserScript(".\\userScripts\\", CONTROLLER_SCRIPT_FILE);
+    engine::EntityHandle padoruHandle = engine.GetGraph()->CreateEntity("Padoru");
+    engine.GetGraph()->CreateComponent<engine::Transform>(padoruHandle)->SetPosition(math::Vector3f(1.5f, 3.0f, 1.0f));
+    engine::Renderer* entityRenderer = engine.GetGraph()->CreateComponent<engine::Renderer>(padoruHandle);
+    engine::Script* controllerScript = engine.GetGraph()->CreateComponent<engine::Script>(padoruHandle);
+
+    controllerScript->AddScriptObject(CONTROLLER_SCRIPT_FILE);
+    controllerScript->Start();
+
+    entityRenderer->SetModel(MODEL_FILE);
+    entityRenderer->SetShader(SHADER_PROGRAM_NAME);
+
+    engine::EntityHandle cameraHandle = engine.GetGraph()->CreateEntity("Camera");
+    engine::Camera* camera = engine.GetGraph()->CreateComponent<engine::Camera>(cameraHandle);
+    engine.GetGraph()->CreateComponent<engine::Transform>(cameraHandle);
+    camera->Rotation() = { -10.5f, 26.5f, 0.0f };
+    camera->Position() = { 1.5f, 3.0f, 3.2f };
+#pragma endregion
+
+    //editor::MenuBar menuBar;
+    //editor::SceneGraphUI sceneGraph("Scene Graph", engine.GetGraph());
+    //editor::PropertyWnd properties(engine.GetGraph());
+    //editor::Viewport* viewport = new editor::Viewport("Viewport", {0.1f, 0.1f, 0.1f, 1.0f});
+
+
+    while (!engine.GetWindow()->ShouldWindowClose())
+    {
+        //engine.Update();
+
+        //// Viewport
+        //viewport->RenderToViewport(engine.GetGraph());
+
+        //// Main menu bar ui
+        //menuBar.Render(engine);
+
+        //// Scene graph window ui
+        //sceneGraph.Render();
+
+        //// Property window ui
+        //if (sceneGraph.IsNewEntitySelected())
+        //    properties.SetHandle(sceneGraph.GetSelectedEntity());
+
+        //properties.Render();
+
+
+        //engine.PostUpdate(viewport);
+
+        engine.UpdateGameplay();
+        engine.UpdateApplicationWindow();
+    }
+
+    //if (viewport)
+    //    delete viewport;
+    engine.ShutDown();
+    return 0;
+}
+
 
 int main(void)
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    {
-        engine::Engine engine;
-        if (engine.Startup("Editor", nullptr))
-            return -1;
-
-        // All the stuff in the following region is temporary & for testing only
-#pragma region LoadTemporaryStuff
-        // Enable the following keys
-        int32 keys[] =
-        {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_W, KEY_S, KEY_A, KEY_D, KEY_ESCAPE};
-
-        for (int32 key : keys)
-            engine::Input::RegisterInput(key);
-        engine::ResourceManager::Load<engine::Model>(MODEL_FILE);
-        engine::ResourceManager::Load<engine::Texture>(TEXTURE_FILE);
-
-        engine::ResourceManager::LoadShader(
-            SHADER_PROGRAM_NAME,
-            SHADER_PROGRAM_VERTEX,
-            SHADER_PROGRAM_FRAGMENT
-        );
-
-        engine::ScriptSystem::CreateUserScript(".\\userScripts\\", CONTROLLER_SCRIPT_FILE);
-        engine::EntityHandle padoruHandle = engine.GetGraph()->CreateEntity("Padoru");
-        engine.GetGraph()->CreateComponent<engine::Transform>(padoruHandle)->SetPosition(math::Vector3f(1.5f, 3.0f, 1.0f));
-        engine::Renderer* entityRenderer = engine.GetGraph()->CreateComponent<engine::Renderer>(padoruHandle);
-        engine::Script* controllerScript = engine.GetGraph()->CreateComponent<engine::Script>(padoruHandle);
-
-        controllerScript->AddScriptObject(CONTROLLER_SCRIPT_FILE);
-        controllerScript->Start();
-
-        entityRenderer->SetModel(MODEL_FILE);
-        entityRenderer->SetShader(SHADER_PROGRAM_NAME);
-
-        engine::EntityHandle cameraHandle = engine.GetGraph()->CreateEntity("Camera");
-        engine::Camera* camera = engine.GetGraph()->CreateComponent<engine::Camera>(cameraHandle);
-        engine.GetGraph()->CreateComponent<engine::Transform>(cameraHandle);
-        camera->Rotation() = {-10.5f, 26.5f, 0.0f};
-        camera->Position() = {1.5f, 3.0f, 3.2f};
-#pragma endregion
-
-        editor::MenuBar menuBar;
-        editor::SceneGraphUI sceneGraph("Scene Graph", engine.GetGraph());
-        editor::PropertyWnd properties(engine.GetGraph());
-        editor::Viewport* viewport = new editor::Viewport("Viewport", {0.1f, 0.1f, 0.1f, 1.0f});
-
-        while (!engine.GetWindow()->ShouldWindowClose())
-        {
-            engine.Update();
-
-            // Viewport
-            viewport->RenderToViewport(engine.GetGraph());
-
-            // Main menu bar ui
-            menuBar.Render(engine);
-
-            // Scene graph window ui
-            sceneGraph.Render();
-
-            // Property window ui
-            if (sceneGraph.IsNewEntitySelected())
-                properties.SetHandle(sceneGraph.GetSelectedEntity());
-
-            properties.Render();
-
-
-            engine.PostUpdate(viewport);
-        }
-
-        if (viewport)
-            delete viewport;
-
-        engine.ShutDown();
-    }
+    
+    int res = appMain();
 
     // Memory leak check
     if (!_CrtDumpMemoryLeaks())
         std::printf("No memory leaks found\n");
 
-    return 0;
+    return res;
 }
 #endif
