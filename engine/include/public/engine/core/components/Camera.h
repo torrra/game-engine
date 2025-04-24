fcc96d61055ee4d2bae4f5924f3f3598ffefda8d
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-04-24 10:58:12 -0400

    Remove constructor from Frustum struct
    Replace constructor with default member values in Frustum struct (Camera.h)

--- engine/include/public/engine/core/components/Camera.h
+++ engine/include/public/engine/core/components/Camera.h
@@ -12,15 +12,10 @@ namespace engine
 {
     struct Frustum
     {
-        f32 m_near;
-        f32 m_far;
-        f32 m_fovRad;
-        f32 m_ratio;
-
-        ENGINE_API Frustum(f32 near = 0.005f, f32 far = 250.0f, f32 fovDeg = 60.0f, f32 ratio = 1.77777777778f)
-            : m_near(near), m_far(far), m_fovRad(fovDeg* DEG2RAD), m_ratio(ratio)
-        {
-        }
+        f32 m_near = 0.005f;
+        f32 m_far = 250.0f;
+        f32 m_fovRad = 1.0471976f;
+        f32 m_ratio = 1.77777777778f;
     };
 
     class Camera : public Component
