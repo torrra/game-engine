762894c0e7a487c05e76a916d45790e94b9a2e8f
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-05-05 09:53:58 -0400

    Make Animation functions private
    Make ReadAnimation and ReadBoneAnim functions private to
    Animation class

--- engine/include/public/engine/resource/animation/Animation.h
+++ engine/include/public/engine/resource/animation/Animation.h
@@ -9,18 +9,23 @@
 
 namespace engine
 {
+    // Position / scale bone transform
     struct Vec3BoneTransform
     {
         math::Vector3f  m_vector = math::Vector3f::One();
+        
+        // Tick index
         int32           m_time = -1;
     };
 
+    // Rotation bone transform
     struct QuatBoneTransform
     {
         math::Quatf    m_quaternion = math::Quatf::Identity();
         int32          m_time = -1;       
     };
 
+    // All frame transformations for a given bone
     class BoneAnimData
     {
 
@@ -54,12 +59,9 @@ namespace engine
         ENGINE_API Animation(void* animData);
                    Animation(const Animation&) = delete;
         ENGINE_API Animation(Animation&&) noexcept = default;
-        ENGINE_API ~Animation(void) noexcept = default;
+        ENGINE_API ~Animation(void) = default;
 
         ENGINE_API bool LoadResource(const char* filePath) override;
-
-        ENGINE_API void ReadAnimation(const void* animation);
-        ENGINE_API void ReadBoneAnim(void* boneAnim);
 
         // When one or several animations are stored with a model,
         // or when several animations are in a single file, load
@@ -67,6 +69,9 @@ namespace engine
         ENGINE_API static void LoadExtraAnimations(const void* anims, uint32 offset = 0);
      
     private:
+
+        void ReadAnimation(const void* animation);
+        void ReadBoneAnim(void* boneAnim);
 
         std::vector<BoneAnimData>   m_animData;
         int32                       m_tickCount = -1;
