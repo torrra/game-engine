9bd7ea8c5f6201306a47dcc0b9dc0030a9c60019
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-05-05 09:36:14 -0400

    Create animation class
    Create class to contain imported assimp animations
    Override LoadResource function from IResource interface
    Create ReadBoneAnim function to reuse import code

---
+++ engine/include/public/engine/resource/animation/Animation.h
@@ -0,0 +1,75 @@
+#pragma once 
+
+#include <vector>
+
+#include "engine/resource/Resource.h"
+
+#include <math/Vector3.hpp>
+#include <math/Quaternion.hpp>
+
+namespace engine
+{
+    struct Vec3BoneTransform
+    {
+        math::Vector3f  m_vector = math::Vector3f::One();
+        int32           m_time = -1;
+    };
+
+    struct QuatBoneTransform
+    {
+        math::Quatf    m_quaternion = math::Quatf::Identity();
+        int32          m_time = -1;       
+    };
+
+    class BoneAnimData
+    {
+
+    public:
+
+        BoneAnimData(void) = default;
+        BoneAnimData(BoneAnimData&&) noexcept = default;
+        BoneAnimData(const BoneAnimData&) = delete;
+        ~BoneAnimData(void) = default;
+
+        BoneAnimData& operator=(BoneAnimData&&) noexcept = default;
+
+    private:
+    
+        std::vector<QuatBoneTransform>  m_rotations;
+        std::vector<Vec3BoneTransform>  m_positions;
+        std::vector<Vec3BoneTransform>  m_scales;
+
+        std::string                     m_boneName;
+
+        friend class Animation;
+    };
+
+
+    class Animation final : public IResource
+    {
+
+    public:
+
+        ENGINE_API Animation(void) = default;
+        ENGINE_API Animation(void* animData);
+                   Animation(const Animation&) = delete;
+        ENGINE_API Animation(Animation&&) noexcept = default;
+        ENGINE_API ~Animation(void) noexcept = default;
+
+        ENGINE_API bool LoadResource(const char* filePath) override;
+
+        ENGINE_API void ReadAnimation(const void* animation);
+        ENGINE_API void ReadBoneAnim(void* boneAnim);
+
+        // When one or several animations are stored with a model,
+        // or when several animations are in a single file, load
+        // secondary animations from that data
+        ENGINE_API static void LoadExtraAnimations(const void* anims, uint32 offset = 0);
+     
+    private:
+
+        std::vector<BoneAnimData>   m_animData;
+        int32                       m_tickCount = -1;
+        float                       m_ticksPerSecond = 0.f;
+    };
+}
---
+++ engine/source/resource/animation/Animation.cpp
@@ -0,0 +1,97 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/anim.h>
+
+#include "resource/ResourceManager.h"
+#include "resource/animation/Animation.h"
+
+namespace engine::importer
+{
+    thread_local Assimp::Importer g_animImporter;
+}
+
+
+namespace engine
+{
+    Animation::Animation(void* animData)
+    {
+        ReadAnimation(animData);
+    }
+
+    bool Animation::LoadResource(const char* filePath)
+    {
+        const aiScene* scene = importer::g_animImporter.ReadFile(filePath, 0);
+
+        if (scene && scene->HasAnimations())
+        {
+            ReadAnimation(scene->mAnimations[0]);
+            LoadExtraAnimations(scene, 1);
+            importer::g_animImporter.FreeScene();
+            return true;
+        }
+
+        return false;
+    }
+
+    void Animation::ReadAnimation(const void* animation)
+    {
+        const aiAnimation* animImpl = reinterpret_cast<const aiAnimation*>(animation);
+
+        m_tickCount = static_cast<int32>(animImpl->mDuration);
+        m_ticksPerSecond = static_cast<f32>(animImpl->mTicksPerSecond);
+        m_animData.reserve(animImpl->mNumChannels);
+
+        for (uint32 boneNum = 0; boneNum < animImpl->mNumChannels; ++boneNum)
+            ReadBoneAnim(animImpl->mChannels[boneNum]);
+    }
+
+    void Animation::ReadBoneAnim(void* boneAnim)
+    {
+        const aiNodeAnim* animNode = reinterpret_cast<const aiNodeAnim*>(boneAnim);
+
+        BoneAnimData& boneData = m_animData.emplace_back();
+
+        boneData.m_boneName = animNode->mNodeName.C_Str();
+        boneData.m_positions.resize(animNode->mNumPositionKeys);
+        boneData.m_rotations.resize(animNode->mNumRotationKeys);
+        boneData.m_scales.resize(animNode->mNumScalingKeys);
+
+        for (uint32 posNum = 0; posNum < animNode->mNumPositionKeys; ++posNum)
+        {
+            Vec3BoneTransform& pos = boneData.m_positions[posNum];
+            aiVectorKey& posKeyData = animNode->mPositionKeys[posNum];
+
+            pos.m_vector = *reinterpret_cast<math::Vector3f*>(&posKeyData.mValue);
+            pos.m_time = static_cast<int32>(posKeyData.mTime);
+        }
+
+        for (uint32 rotationNum = 0; rotationNum < animNode->mNumRotationKeys; ++rotationNum)
+        {
+            QuatBoneTransform& rotation = boneData.m_rotations[rotationNum];
+            aiQuatKey& rotationKeyData = animNode->mRotationKeys[rotationNum];
+
+            rotation.m_quaternion = *reinterpret_cast<math::Quatf*>(&rotationKeyData.mValue);
+            rotation.m_time = static_cast<int32>(rotationKeyData.mTime);
+        }
+
+        for (uint32 scaleNum = 0; scaleNum < animNode->mNumScalingKeys; ++scaleNum)
+        {
+            Vec3BoneTransform& scale = boneData.m_scales[scaleNum];
+            aiVectorKey& scaleKeyData = animNode->mScalingKeys[scaleNum];
+
+            scale.m_vector = *reinterpret_cast<math::Vector3f*>(&scaleKeyData.mValue);
+            scale.m_time = static_cast<int32>(scaleKeyData.mTime);
+        }
+    }
+
+    void Animation::LoadExtraAnimations(const void* anims, uint32 offset)
+    {
+        const aiScene* scene = reinterpret_cast<const aiScene*>(anims);
+
+        for (uint32 animNum = offset; animNum < scene->mNumAnimations; ++animNum)
+        {
+            const aiAnimation* anim = scene->mAnimations[animNum];
+            ResourceManager::CreateFromData<Animation>(anim->mName.C_Str(), (void*)anim);
+        }
+    }
+}
