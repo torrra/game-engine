f9835930a378d8c872b5a819a3a4b18c3f031dcb
Justine <jus.martin@student.isartdigital.com>
2025-05-07 11:13:47 -0400

    Implement SimulcationEventCallback class

---
+++ engine/include/public/engine/physics/SimulationEventCallback.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#pragma region PhysX
+
+#include <physx/PxPhysicsAPI.h>
+
+#pragma endregion
+
+namespace engine
+{
+    class SimulationEventCallback
+    {
+    public :
+
+        void onContact(const physx::PxContactPairHeader& inPairHeader,
+                       const physx::PxContactPair* inPairs,
+                       physx::PxU32 inNbPairs) override;
+
+        void onTrigger(physx::PxTriggerPair* inPairs,
+                       physx::PxU32 inNbPairs) override;
+
+        void onAdvance(const physx::PxRigidBody* const* bodyBuffer,
+                       const physx::PxTransform* poseBuffer,
+                       const physx::PxU32 count) override;
+
+        void onConstraintBreak(physx::PxConstraintInfo* constraints,
+                               physx::PxU32 count) override;
+
+        void onWake(physx::PxActor** actors,
+                    physx::PxU32 count) override;
+
+        void onSleep(physx::PxActor** actors,
+                     physx::PxU32 count) override;
+
+    }; // !Class SimulationEventCallback
+} // !Namespace engine
