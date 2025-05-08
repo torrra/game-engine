9a4ff7995e15a5e37d647217dbc95a75e6daefb8
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-05-07 21:42:23 -0400

    Add Clamp and DenyAccess lua functions
    Add a clamp math function in lua Utils table, as well as a function to
    deny access to Utils invalid indices in Utils.lua

--- workspace/scripts/Utils.lua
+++ workspace/scripts/Utils.lua
@@ -4,3 +4,27 @@ function Utils.AlmostEqual(valA, valB)
 
     return math.abs(valA - valB) < 0.00001
 end
+
+function Utils.Clamp(val, low, high)
+
+    if val < low then 
+        return low
+
+    elseif val > high then
+        return high
+
+    else
+        return val
+    end
+end
+
+function Utils.DenyAccess(_, val)
+    error("[Utils] Cannot access index "..tostring(val).." in this table")
+end
+
+Utils.__index = Utils.DenyAccess
+Utils.__newindex = Utils.DenyAccess
+Utils.__metatable = "Protected metatable"
+setmetatable(Utils, Utils)
+
+return Utils
