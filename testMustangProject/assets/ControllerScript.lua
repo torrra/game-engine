8f7e17a60052dde7834d5f21f771abe26420e61c
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-05-12 10:15:47 -0400

    Shoot raycast in ControllerScript.lua

--- testMustangProject/assets/ControllerScript.lua
+++ testMustangProject/assets/ControllerScript.lua
@@ -1,16 +1,26 @@
 ControllerScript = ScriptObject:_new()
 
 -- Define member variables here
+local distance = 10.0
 
 -- Is executed once when the object becomes active
 function ControllerScript:Start()
     self.transform = GetTransformComponent(self.entity.handle)
+    self.gunRay = Raycast.new()
+    self.gunRay:SetDistance(distance)
+
+    print(self.gunRay:GetDistance())
+
+    local cameraEntity = GetEntity("Camera")
+
+    self.camera = GetCameraComponent(cameraEntity.handle)
+
 end
 
 
 -- Is executed every tick
 function ControllerScript:Update(deltaTime)
-    if IsInputDown(InputCode.KEY_W) then
+    --[[ if IsInputDown(InputCode.KEY_W) then
         self.transform:AddTranslation(0, 0, 1 * deltaTime) 
     
     elseif IsInputDown(InputCode.KEY_S) then
@@ -22,6 +32,24 @@ function ControllerScript:Update(deltaTime)
     
     elseif IsInputDown(InputCode.KEY_D) then
         self.transform:AddTranslation(-1 * deltaTime, 0, 0)
+    end
+    --]]
+
+    if (IsInputDown(InputCode.MOUSE_BUTTON_LEFT)) then
+        
+        local direction = Vector3.new(0.0, 0.0, -1):Rotate(self.camera:GetRotation())
+        local position = Vector3.new(self.camera:GetPosition())
+
+        print("direction: "..direction)
+        print("origin: "..position)
+
+        self.gunRay:SetOrigin(position)
+        self.gunRay:SetDirection(direction)
+
+        local result = self.gunRay:HasHit()
+
+        print(result.hasHit)
+
     end
 end
 
