e80c79e3c026cb5abb2236fe587897aa2b825d9e
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-05-12 10:07:37 -0400

    Return camera rotation in radians
    Return camera rotation in radians in script_GetCameraRotation function

--- engine/source/scripting/ecs/CameraFunctions.cpp
+++ engine/source/scripting/ecs/CameraFunctions.cpp
@@ -103,9 +103,9 @@ int script_GetCameraRotation(lua_State* luaState)
     {
         math::Vector3f camEulers = camera->GetRotation();
 
-        lua_pushnumber(luaState, static_cast<lua_Number>(math::Degree(camEulers.GetX()).Deg()));
-        lua_pushnumber(luaState, static_cast<lua_Number>(math::Degree(camEulers.GetY()).Deg()));
-        lua_pushnumber(luaState, static_cast<lua_Number>(math::Degree(camEulers.GetZ()).Deg()));
+        lua_pushnumber(luaState, static_cast<lua_Number>(math::Degree(camEulers.GetX()).Rad()));
+        lua_pushnumber(luaState, static_cast<lua_Number>(math::Degree(camEulers.GetY()).Rad()));
+        lua_pushnumber(luaState, static_cast<lua_Number>(math::Degree(camEulers.GetZ()).Rad()));
     }
 
     return 3;
