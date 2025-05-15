f4d6d5afed054147cf1bf1fde772582ca3ca736f
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-05-15 14:26:43 -0400

    Store script object index and unregister
    Store the script object's index withon the script component's array and add a function to unregister it

--- engine/include/public/engine/scripting/ScriptObject.h
+++ engine/include/public/engine/scripting/ScriptObject.h
@@ -13,19 +13,30 @@ namespace engine
     public:
 
         ScriptObject(void) = delete;
-        ScriptObject(EntityHandle entity, const std::string& type);
-        ScriptObject(const ScriptObject&) = default;
-        ~ScriptObject(void) = default;
+        ENGINE_API ScriptObject(EntityHandle entity, const std::string& type, uint32 index);
+        ENGINE_API ScriptObject(ScriptObject&&) noexcept = default;
+                   ScriptObject(const ScriptObject&) = delete;
+        ENGINE_API ~ScriptObject(void) = default;
 
         ENGINE_API const std::string& GetType(void) const;
 
         // Create lua instance
-        void Register(void) const;
+        ENGINE_API void Register(void) const;
+
+        ENGINE_API void Unregister(void);
+
+        ENGINE_API bool IsValid(void) const;
+        ENGINE_API uint32 GetIndex(void) const;
+        ENGINE_API const std::string& GetName(void) const;
+
+        ENGINE_API ScriptObject& operator=(ScriptObject&&) noexcept = default;
+        ENGINE_API ScriptObject& operator=(const ScriptObject&) = delete;
 
     private:
 
-        // TODO: change for script resource when ready
         std::string		m_type;
         EntityHandle	m_entity;
+        uint32          m_index;
+        bool            m_isValid;
     };
 }
