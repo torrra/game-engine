a880f864cbd04dee19797914bb698faabe670762
Justine <jus.martin@student.isartdigital.com>
2025-05-20 15:50:42 -0400

    Implement NavigationPointComponent class

--- editor/include/ui/components/NavigationPointComponent.h
+++ editor/include/ui/components/NavigationPointComponent.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#pragma region Editor
+
+#include "Component.h"
+
+#pragma endregion
+
 namespace editor
 {
-    class 
+    class NavigationPointComponent : public BaseComponent
+    {
+    public:
+
+        /// Constructor
+                        NavigationPointComponent(void);
+
+        /// Destructor
+                        ~NavigationPointComponent(void);
+
+    protected:
+
+        virtual void    SectionContent(void) override;
+    }; // !Class NavigationPointComponent
 } // !Namespace editor
--- editor/source/ui/components/NavigationPointComponent.cpp
+++ editor/source/ui/components/NavigationPointComponent.cpp
@@ -1,1 +1,16 @@
 #include "ui/components/NavigationPointComponent.h"
+
+editor::NavigationPointComponent::NavigationPointComponent(void)
+{
+
+}
+
+editor::NavigationPointComponent::~NavigationPointComponent(void)
+{
+
+}
+
+void editor::NavigationPointComponent::SectionContent(void)
+{
+
+}
