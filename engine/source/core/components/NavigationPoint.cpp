f13167234c768168175763c8e575da2b3dda1d60
Justine <jus.martin@student.isartdigital.com>
2025-05-20 15:35:50 -0400

    Implement the constructor

--- engine/source/core/components/NavigationPoint.cpp
+++ engine/source/core/components/NavigationPoint.cpp
@@ -5,3 +5,25 @@
 #include "serialization/TextSerializer.h"
 
 #pragma endregion
+
+#pragma region Internal
+
+#include "InternalOpenGLError.hpp"
+
+#pragma endregion
+
+#pragma region Resource
+
+#include "engine/resource/shader/Shader.h"
+#include "engine/resource/ResourceManager.h"
+
+#pragma endregion
+
+engine::NavigationPoint::NavigationPoint(EntityHandle inOwner, SceneGraph* inScene)
+{
+    inOwner = m_owner;
+    inScene = m_currentScene;
+}
+
+}
+
