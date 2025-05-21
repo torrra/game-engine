5d4c4546c40987c9f519376bca52fd9c904628f7
Justine <jus.martin@student.isartdigital.com>
2025-05-20 21:58:28 -0400

    Implement Draw overloaded
    Without parameters

--- engine/include/public/engine/resource/model/Model.h
+++ engine/include/public/engine/resource/model/Model.h
@@ -23,6 +23,7 @@ namespace engine
 
         ENGINE_API bool IsDynamic(void) const;
 
+        ENGINE_API void Draw(void) const;
         ENGINE_API void Draw(const std::vector<const MeshMaterial*>& materials) const;
 
         ENGINE_API uint32 GetMeshCount(void) const;
