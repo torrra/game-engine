99a212d681c3c7257eb3bcb7079539e15a35cafb
Marc Levesque <m.levesque@student.isartdigital.com>
2025-05-29 15:39:14 -0400

    Add features to menu bar
    Add save & build options to menu bar

--- editor/source/ui/MenuBar.cpp
+++ editor/source/ui/MenuBar.cpp
@@ -77,6 +77,11 @@ void editor::MenuBar::ProjectMenu(::engine::GameScene& scene)
     
     if (ui::StartMenu("Project"))
     {
+        if (ui::MenuItem("Save project", "Ctrl+S"))
+        {
+            engine::Engine::GetEngine()->SaveProject();
+        }
+
         if (ui::MenuItem("Create project"))
         {
             m_isCreateWndOpened = true;
@@ -85,6 +90,11 @@ void editor::MenuBar::ProjectMenu(::engine::GameScene& scene)
         if (ui::MenuItem("Open project"))
         {
             OpenProject(scene);
+        }
+
+        if (ui::MenuItem("Build project", "Ctrl+B"))
+        {
+            engine::Engine::GetEngine()->BuildProjectExecutable("..\\testProjectBuildFolder");
         }
 
         ui::EndMenu();
