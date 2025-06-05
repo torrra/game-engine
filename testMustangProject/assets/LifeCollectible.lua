b0f59a0ba8fa5671aba4328ac31416f793691e45
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-06-05 09:06:54 -0400

    Add Collectible derived classes
    Create LifeCollectible and LightAmmoCollectible.lua derived from
    Collectible

---
+++ testMustangProject/assets/LifeCollectible.lua
@@ -0,0 +1,22 @@
+require ("Collectible")
+
+LifeCollectible = Collectible:new()
+
+LifeCollectible.type = CollectibleType.LIFE
+LifeCollectible.value = 1
+
+-- Is executed once when the object becomes active
+function LifeCollectible:Start()
+
+end
+
+
+-- Is executed every tick
+function LifeCollectible:Update(deltaTime)
+
+end
+
+
+-- Engine definitions
+ScriptObjectTypes.LifeCollectible = LifeCollectible
+return LifeCollectible
