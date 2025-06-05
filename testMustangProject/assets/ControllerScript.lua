6ee55221910b4210c81fb37e8facc8c194182aa5
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-06-05 09:07:36 -0400

    Add Reload to ControllerScript
    Add key to reload gun in ControllerScript.lua

--- testMustangProject/assets/ControllerScript.lua
+++ testMustangProject/assets/ControllerScript.lua
@@ -6,21 +6,20 @@ local distance = 10.0
 -- Is executed once when the object becomes active
 function ControllerScript:Start()
     self.transform = GetTransformComponent(self.entity.handle)
-    self.gunRay = Raycast.new()
-    self.gunRay:SetDistance(distance)
-
-    print(self.gunRay:GetDistance())
 
     local cameraEntity = GetEntity("Camera")
 
     self.camera = GetCameraComponent(cameraEntity.handle)
+
+    local script = GetScriptComponent(self.entity.handle)
+    self.gun = script.BaseGun
 
 end
 
 
 -- Is executed every tick
 function ControllerScript:Update(deltaTime)
-    --[[ if IsInputDown(InputCode.KEY_W) then
+     --[[if IsInputDown(InputCode.KEY_W) then
         self.transform:AddTranslation(0, 0, 1 * deltaTime) 
     
     elseif IsInputDown(InputCode.KEY_S) then
@@ -33,18 +32,25 @@ function ControllerScript:Update(deltaTime)
     elseif IsInputDown(InputCode.KEY_D) then
         self.transform:AddTranslation(-1 * deltaTime, 0, 0)
     end
-    --]]
+    ]]--
 
-    if (IsInputDown(InputCode.MOUSE_BUTTON_LEFT)) then
-        
-        local direction = Vector3.new(0.0, 0.0, -1):Rotate(self.camera:GetRotation())
-        local position = Vector3.new(self.camera:GetPosition())
-        
-        self.gunRay:SetOrigin(position)
-        self.gunRay:SetDirection(direction)
-
-        local result = self.gunRay:HasHit()
+    if IsInputDown(InputCode.KEY_Q) then
+        self.transform:AddEulerRotation(0, 90 * deltaTime, 0)
+    elseif IsInputDown(InputCode.KEY_E) then
+        self.transform:AddEulerRotation(0, -90 * deltaTime, 0)
     end
+    
+    --self.transform:SetRotation(self.camera:GetRotation())
+
+    if (IsInputPressed(InputCode.MOUSE_BUTTON_LEFT)) then
+        
+        self.gun:Fire()
+    end
+    
+    if IsInputPressed(InputCode.KEY_R) then
+        self.gun:Reload()
+    end
+
 end
 
 function ControllerScript:OnCollisionEnter(otherEntity)
