1b508ee72e2e0077bc5509608d081631f84b0804
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2025-06-05 17:43:46 -0400

    Add ResourceFunctions
    Create LoadResource function to load a resource from lua

--- workspace/scripts/Utils.lua
+++ workspace/scripts/Utils.lua
@@ -1,5 +1,16 @@
 Utils = {}
 
+ResourceTypes =
+{
+    MODEL = 0,
+    SHADER = 1,
+    AUDIO = 2,
+    TEXTURE = 3,
+    FONT = 4,
+    ANIMATION = 5,
+    MATERIAL = 6,
+}
+
 function Utils.AlmostEqual(valA, valB)
 
     return math.abs(valA - valB) < 0.00001
@@ -21,6 +32,12 @@ end
 function Utils.DenyAccess(_, val)
     error("[Utils] Cannot access index "..tostring(val).." in this table")
 end
+
+function LoadResource(type, name)
+
+    ResourceC.LoadResource(type, name)
+end
+
 
 Utils.__index = Utils.DenyAccess
 Utils.__newindex = Utils.DenyAccess
