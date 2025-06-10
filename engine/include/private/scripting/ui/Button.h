dcbb4c7ca32633786c2917cf3c6e6582c5b16cd2
Marc Levesque <m.levesque@student.isartdigital.com>
2025-06-10 09:17:07 -0400

    Add button size setter
    Add lua button size function

--- engine/include/private/scripting/ui/Button.h
+++ engine/include/private/scripting/ui/Button.h
@@ -6,6 +6,7 @@ extern "C"
     int script_SetColor(struct lua_State* luaState);
     int script_SetHoveredColor(struct lua_State* luaState);
     int script_SetPressedColor(struct lua_State* luaState);
+    int script_SetButtonSize(struct lua_State* luaState);
 }
 
 namespace engine
